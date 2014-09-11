e9de7151e24c441b677e333713fda95cad1d7347
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2014-09-11 13:29:25 -0700

    rfnoc: Added FFT windowing RFNoC block

--- usrp3/top/x300/rfnoc_ce_auto_inst.v
+++ usrp3/top/x300/rfnoc_ce_auto_inst.v
@@ -48,19 +48,29 @@
     .o_tdata(ce_i_tdata[2]), .o_tlast(ce_i_tlast[2]), .o_tvalid(ce_i_tvalid[2]), .o_tready(ce_i_tready[2]),
     .debug(ce_debug[2]));
     
+  noc_block_window #(
+    .NOC_ID(64'hD053_0000_0000_0000),
+    .STR_SINK_FIFOSIZE(11))
+  inst_noc_block_window (
+    .bus_clk(bus_clk), .bus_rst(bus_rst),
+    .ce_clk(bus_clk), .ce_rst(bus_rst),
+    .i_tdata(ce_o_tdata[3]), .i_tlast(ce_o_tlast[3]), .i_tvalid(ce_o_tvalid[3]), .i_tready(ce_o_tready[3]),
+    .o_tdata(ce_i_tdata[3]), .o_tlast(ce_i_tlast[3]), .o_tvalid(ce_i_tvalid[3]), .o_tready(ce_i_tready[3]),
+    .debug(ce_debug[3]));
+    
   noc_block_null_source_sink #(
     .NOC_ID(64'h0000_0000_0000_0000),
     .STR_SINK_FIFOSIZE(11))
   inst_noc_block_null_source_sink (
     .bus_clk(bus_clk), .bus_rst(bus_rst),
     .ce_clk(bus_clk), .ce_rst(bus_rst),
-    .i_tdata(ce_o_tdata[3]), .i_tlast(ce_o_tlast[3]), .i_tvalid(ce_o_tvalid[3]), .i_tready(ce_o_tready[3]),
-    .o_tdata(ce_i_tdata[3]), .o_tlast(ce_i_tlast[3]), .o_tvalid(ce_i_tvalid[3]), .o_tready(ce_i_tready[3]),
-    .debug(ce_debug[3]));
+    .i_tdata(ce_o_tdata[4]), .i_tlast(ce_o_tlast[4]), .i_tvalid(ce_o_tvalid[4]), .i_tready(ce_o_tready[4]),
+    .o_tdata(ce_i_tdata[4]), .o_tlast(ce_i_tlast[4]), .o_tvalid(ce_i_tvalid[4]), .o_tready(ce_i_tready[4]),
+    .debug(ce_debug[4]));
   
   genvar n;
   generate
-    for (n = 4; n < NUM_CE; n = n + 1) begin
+    for (n = 0; n < NUM_CE-5; n = n + 1) begin
       noc_block_axi_fifo_loopback #(
         .NOC_ID(64'hF1F0_0000_0000_0000 + n),
         .STR_SINK_FIFOSIZE(11))
