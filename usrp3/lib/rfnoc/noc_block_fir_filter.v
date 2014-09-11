eecfad9223b2f282ea04674180c16926ab60dc3a
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2014-09-11 12:26:13 -0700

    rfnoc: Added support for reconfigurable coefficients to FIR filter block

--- usrp3/lib/rfnoc/noc_block_fir_filter.v
+++ usrp3/lib/rfnoc/noc_block_fir_filter.v
@@ -1,5 +1,5 @@
 module noc_block_fir_filter #(
-  parameter NOC_ID = 64'h0000_0002_0000_0000,
+  parameter NOC_ID = 64'hF112_0000_0000_0000,
   parameter STR_SINK_FIFOSIZE = 10)
 (
   input bus_clk, input bus_rst,
@@ -9,14 +9,17 @@ module noc_block_fir_filter #(
   output [63:0] debug
 );
 
-  /////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////
   //
   // RFNoC Shell
   //
   ////////////////////////////////////////////////////////////
+  localparam SR_READBACK = 255;
+  
   wire [31:0] set_data;
   wire [7:0]  set_addr;
   wire        set_stb;
+  reg  [63:0] rb_data;
 
   wire [63:0] cmdout_tdata, ackin_tdata;
   wire        cmdout_tlast, cmdout_tvalid, cmdout_tready, ackin_tlast, ackin_tvalid, ackin_tready;
@@ -34,7 +37,7 @@ module noc_block_fir_filter #(
     // Computer Engine Clock Domain
     .clk(ce_clk), .reset(ce_rst),
     // Control Sink
-    .set_data(set_data), .set_addr(set_addr), .set_stb(set_stb), .rb_data(64'd0),
+    .set_data(set_data), .set_addr(set_addr), .set_stb(set_stb), .rb_data(rb_data),
     // Control Source
     .cmdout_tdata(cmdout_tdata), .cmdout_tlast(cmdout_tlast), .cmdout_tvalid(cmdout_tvalid), .cmdout_tready(cmdout_tready),
     .ackin_tdata(ackin_tdata), .ackin_tlast(ackin_tlast), .ackin_tvalid(ackin_tvalid), .ackin_tready(ackin_tready),
@@ -44,58 +47,118 @@ module noc_block_fir_filter #(
     .str_src_tdata(str_src_tdata), .str_src_tlast(str_src_tlast), .str_src_tvalid(str_src_tvalid), .str_src_tready(str_src_tready),
     .debug(debug));
 
-  // Control Source Unused
-  assign cmdout_tdata = 64'd0;
-  assign cmdout_tlast = 1'b0;
-  assign cmdout_tvalid = 1'b0;
-  assign ackin_tready = 1'b1;
-
-  /////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////
   //
-  // User code
+  // AXI Wrapper
+  // Convert RFNoC Shell interface into AXI stream interface
   //
   ////////////////////////////////////////////////////////////
+  localparam NUM_AXI_CONFIG_BUS = 2;
+  
+  wire [31:0] m_axis_data_tdata;
+  wire        m_axis_data_tlast;
+  wire        m_axis_data_tvalid;
+  wire        m_axis_data_tready;
+  
+  wire [31:0] s_axis_data_tdata;
+  wire        s_axis_data_tlast;
+  wire        s_axis_data_tvalid;
+  wire        s_axis_data_tready;
 
-  wire [31:0] pre_tdata, post_tdata;
-  wire        pre_tlast, post_tlast, pre_tvalid, post_tvalid, pre_tready, post_tready;
-
-  wire [31:0] axis_config_tdata1;
-  wire        axis_config_tvalid1, axis_config_tready1;
-
+  
+  wire [NUM_AXI_CONFIG_BUS*32-1:0] m_axis_config_tdata;
+  wire [31:0] m_axis_config_tdata_array[0:NUM_AXI_CONFIG_BUS-1];
+  wire [NUM_AXI_CONFIG_BUS-1:0] m_axis_config_tlast;
+  wire [NUM_AXI_CONFIG_BUS-1:0] m_axis_config_tvalid;
+  wire [NUM_AXI_CONFIG_BUS-1:0] m_axis_config_tready;
+  
+  // Create an array of configuration busses
+  genvar k;
+  generate
+    for (k = 0; k < NUM_AXI_CONFIG_BUS; k = k + 1) begin
+        assign m_axis_config_tdata_array[k] = m_axis_config_tdata[k*32+31:k*32];
+    end
+  endgenerate
+  
   axi_wrapper #(
-    .BASE(8))
+    .BASE(8),
+    .NUM_AXI_CONFIG_BUS(NUM_AXI_CONFIG_BUS))
   inst_axi_wrapper (
     .clk(ce_clk), .reset(ce_rst),
     .set_stb(set_stb), .set_addr(set_addr), .set_data(set_data),
     .i_tdata(str_sink_tdata), .i_tlast(str_sink_tlast), .i_tvalid(str_sink_tvalid), .i_tready(str_sink_tready),
     .o_tdata(str_src_tdata), .o_tlast(str_src_tlast), .o_tvalid(str_src_tvalid), .o_tready(str_src_tready),
-    .m_axis_data_tdata(pre_tdata),
-    .m_axis_data_tlast(pre_tlast),
-    .m_axis_data_tvalid(pre_tvalid),
-    .m_axis_data_tready(pre_tready),
-    .s_axis_data_tdata(post_tdata),
-    .s_axis_data_tlast(post_tlast),
-    .s_axis_data_tvalid(post_tvalid),
-    .s_axis_data_tready(post_tready),
-    .m_axis_config_tdata(axis_config_tdata1),
-    .m_axis_config_tvalid(axis_config_tvalid1),
-    .m_axis_config_tready(axis_config_tready1));
+    .m_axis_data_tdata(m_axis_data_tdata),
+    .m_axis_data_tlast(m_axis_data_tlast),
+    .m_axis_data_tvalid(m_axis_data_tvalid),
+    .m_axis_data_tready(m_axis_data_tready),
+    .s_axis_data_tdata(s_axis_data_tdata),
+    .s_axis_data_tlast(s_axis_data_tlast),
+    .s_axis_data_tvalid(s_axis_data_tvalid),
+    .s_axis_data_tready(s_axis_data_tready),
+    .m_axis_config_tdata(m_axis_config_tdata),
+    .m_axis_config_tlast(m_axis_config_tlast),
+    .m_axis_config_tvalid(m_axis_config_tvalid), 
+    .m_axis_config_tready(m_axis_config_tready));
+  
+  ////////////////////////////////////////////////////////////
+  //
+  // User code
+  //
+  ////////////////////////////////////////////////////////////
+  
+  // Control Source Unused
+  assign cmdout_tdata = 64'd0;
+  assign cmdout_tlast = 1'b0;
+  assign cmdout_tvalid = 1'b0;
+  assign ackin_tready = 1'b1;
+  
+  localparam NUM_TAPS = 41;
+  localparam RB_ADDR_WIDTH = 3;
+  
+  wire [RB_ADDR_WIDTH-1:0] rb_addr;
+  setting_reg #(
+    .my_addr(SR_READBACK), .awidth(8), .width(RB_ADDR_WIDTH)) 
+  sr_rdback (
+    .clk(ce_clk), .rst(ce_rst),
+    .strobe(set_stb), .addr(set_addr), .in(set_data), .out(rb_addr), .changed());
+  
+  // Readback register for number of FIR filter taps
+  always @*
+    case(rb_addr)
+      3'd0    : rb_data <= {NUM_TAPS};
+      default : rb_data <= 64'h0BADC0DE0BADC0DE;
+  endcase
+  
+  // AXI configuration bus 0
+  wire [31:0] m_axis_fir_reload_tdata  = m_axis_config_tdata_array[0];
+  wire        m_axis_fir_reload_tvalid = m_axis_config_tvalid[0];
+  wire        m_axis_fir_reload_tready;
+  assign      m_axis_config_tready[0]  = m_axis_fir_reload_tready;
+  wire        m_axis_fir_reload_tlast  = m_axis_config_tlast[0];
+
+  // AXI configuration bus 1
+  wire [7:0]  m_axis_fir_config_tdata  = m_axis_config_tdata_array[1][7:0];
+  wire        m_axis_fir_config_tvalid = m_axis_config_tvalid[1];
+  wire        m_axis_fir_config_tready;
+  assign      m_axis_config_tready[1]  = m_axis_fir_config_tready;
 
   simple_fir inst_simple_fir (
     .aresetn(~ce_rst), .aclk(ce_clk),
-    .s_axis_data_tvalid(pre_tvalid),
-    .s_axis_data_tready(pre_tready),
-    .s_axis_data_tlast(pre_tlast),
-    .s_axis_data_tdata(pre_tdata),
-    .m_axis_data_tvalid(post_tvalid),
-    .m_axis_data_tready(post_tready),
-    .m_axis_data_tlast(post_tlast),
-    .m_axis_data_tdata(post_tdata),
-    .s_axis_config_tdata(axis_config_tdata1),
-    .s_axis_config_tvalid(axis_config_tvalid1),
-    .s_axis_config_tready(axis_config_tready1),
-    .s_axis_reload_tdata(0),
-    .s_axis_reload_tvalid(1'b0),
-    .s_axis_reload_tlast(1'b0));
+    .s_axis_data_tdata(m_axis_data_tdata),
+    .s_axis_data_tlast(m_axis_data_tlast),
+    .s_axis_data_tvalid(m_axis_data_tvalid),
+    .s_axis_data_tready(m_axis_data_tready),
+    .m_axis_data_tdata(s_axis_data_tdata),
+    .m_axis_data_tlast(s_axis_data_tlast),
+    .m_axis_data_tvalid(s_axis_data_tvalid),
+    .m_axis_data_tready(s_axis_data_tready),
+    .s_axis_config_tdata(m_axis_fir_config_tdata),
+    .s_axis_config_tvalid(m_axis_fir_config_tvalid),
+    .s_axis_config_tready(m_axis_fir_config_tready),
+    .s_axis_reload_tdata(m_axis_fir_reload_tdata),
+    .s_axis_reload_tvalid(m_axis_fir_reload_tvalid),
+    .s_axis_reload_tready(m_axis_fir_reload_tready),
+    .s_axis_reload_tlast(m_axis_fir_reload_tlast));
 
 endmodule
