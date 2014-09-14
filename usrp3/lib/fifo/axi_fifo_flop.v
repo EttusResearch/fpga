e9a2bca7fb9d81626cc2fa6c9823b7c02fffc97b
Matt Ettus <matt@ettus.com>
2014-09-13 16:42:22 -1000

    axi: single entry axi fifo, just a flop.  Can be useful for timing purposes or to encapsulate proper AXIS logic.

---
+++ usrp3/lib/fifo/axi_fifo_flop.v
@@ -0,0 +1,34 @@
+//
+// Copyright 2014 Ettus Research LLC
+//
+
+
+//
+// Single FIFO (register) with AXI4-STREAM interface
+//
+
+module axi_fifo_flop
+  #(parameter WIDTH=32)
+   (input clk, 
+    input reset, 
+    input clear,
+    input [WIDTH-1:0] i_tdata,
+    input i_tvalid,
+    output i_tready,
+    output reg [WIDTH-1:0] o_tdata,
+    output reg o_tvalid,
+    input o_tready);
+
+   assign i_tready = ~o_tvalid | o_tready;
+
+   always @(posedge clk)
+     if(reset | clear)
+       o_tvalid <= 1'b0;
+     else
+       o_tvalid <= (i_tready & i_tvalid) | (o_tvalid & ~o_tready);
+
+   always @(posedge clk)
+     if(i_tvalid & i_tready)
+       o_tdata <= i_tdata;
+   
+endmodule // axi_fifo_flop
