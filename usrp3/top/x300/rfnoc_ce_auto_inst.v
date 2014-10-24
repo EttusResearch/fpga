ae1c52036a3a070286f8b1841c16384058a4b3ae
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2014-10-24 10:58:46 -0700

    rfnoc: Updated default instantiated RFNoC blocks

--- usrp3/top/x300/rfnoc_ce_auto_inst.v
+++ usrp3/top/x300/rfnoc_ce_auto_inst.v
@@ -1,10 +1,10 @@
-  localparam NUM_CE = 11;
+  localparam NUM_CE = 8;
   
   wire [NUM_CE*64-1:0] ce_flat_o_tdata, ce_flat_i_tdata;
   wire [63:0]          ce_o_tdata[0:NUM_CE-1], ce_i_tdata[0:NUM_CE-1];
   wire [NUM_CE-1:0]    ce_o_tlast, ce_o_tvalid, ce_o_tready, ce_i_tlast, ce_i_tvalid, ce_i_tready;
   wire [63:0]          ce_debug[0:NUM_CE-1];
-      
+
   // Flattern CE tdata arrays
   genvar k;
   generate
@@ -13,61 +13,60 @@
       assign ce_flat_i_tdata[k*64+63:k*64] = ce_i_tdata[k];
     end
   endgenerate
-  
+
   // Automatically instantiated CEs
-  noc_block_schmidl_cox #(
-    .NOC_ID(64'h5CC0_0000_0000_0000),
-    .STR_SINK_FIFOSIZE(11))
-  inst_noc_block_schmidl_cox (
+  noc_block_addsub inst_noc_block_addsub (
     .bus_clk(bus_clk), .bus_rst(bus_rst),
     .ce_clk(bus_clk), .ce_rst(bus_rst),
     .i_tdata(ce_o_tdata[0]), .i_tlast(ce_o_tlast[0]), .i_tvalid(ce_o_tvalid[0]), .i_tready(ce_o_tready[0]),
     .o_tdata(ce_i_tdata[0]), .o_tlast(ce_i_tlast[0]), .o_tvalid(ce_i_tvalid[0]), .o_tready(ce_i_tready[0]),
     .debug(ce_debug[0]));
-  
-  noc_block_fir_filter #(
-    .NOC_ID(64'hF112_0000_0000_0000),
-    .STR_SINK_FIFOSIZE(11))
-  inst_noc_block_fir_filter (
+
+  noc_block_fir_filter inst_noc_block_fir_filter (
     .bus_clk(bus_clk), .bus_rst(bus_rst),
     .ce_clk(bus_clk), .ce_rst(bus_rst),
     .i_tdata(ce_o_tdata[1]), .i_tlast(ce_o_tlast[1]), .i_tvalid(ce_o_tvalid[1]), .i_tready(ce_o_tready[1]),
     .o_tdata(ce_i_tdata[1]), .o_tlast(ce_i_tlast[1]), .o_tvalid(ce_i_tvalid[1]), .o_tready(ce_i_tready[1]),
     .debug(ce_debug[1]));
 
-  noc_block_fft #(
-    .NOC_ID(64'hFF70_0000_0000_0000),
-    .STR_SINK_FIFOSIZE(11))
-  inst_noc_block_fft (
+  noc_block_fft inst_noc_block_fft (
     .bus_clk(bus_clk), .bus_rst(bus_rst),
     .ce_clk(bus_clk), .ce_rst(bus_rst),
     .i_tdata(ce_o_tdata[2]), .i_tlast(ce_o_tlast[2]), .i_tvalid(ce_o_tvalid[2]), .i_tready(ce_o_tready[2]),
     .o_tdata(ce_i_tdata[2]), .o_tlast(ce_i_tlast[2]), .o_tvalid(ce_i_tvalid[2]), .o_tready(ce_i_tready[2]),
     .debug(ce_debug[2]));
-    
-  noc_block_window #(
-    .NOC_ID(64'hD053_0000_0000_0000),
-    .STR_SINK_FIFOSIZE(11))
-  inst_noc_block_window (
+
+  noc_block_window inst_noc_block_window (
     .bus_clk(bus_clk), .bus_rst(bus_rst),
     .ce_clk(bus_clk), .ce_rst(bus_rst),
     .i_tdata(ce_o_tdata[3]), .i_tlast(ce_o_tlast[3]), .i_tvalid(ce_o_tvalid[3]), .i_tready(ce_o_tready[3]),
     .o_tdata(ce_i_tdata[3]), .o_tlast(ce_i_tlast[3]), .o_tvalid(ce_i_tvalid[3]), .o_tready(ce_i_tready[3]),
     .debug(ce_debug[3]));
-    
-  noc_block_null_source_sink #(
-    .NOC_ID(64'h0000_0000_0000_0000),
-    .STR_SINK_FIFOSIZE(11))
-  inst_noc_block_null_source_sink (
+
+  noc_block_null_source_sink inst_noc_block_null_source_sink (
     .bus_clk(bus_clk), .bus_rst(bus_rst),
     .ce_clk(bus_clk), .ce_rst(bus_rst),
     .i_tdata(ce_o_tdata[4]), .i_tlast(ce_o_tlast[4]), .i_tvalid(ce_o_tvalid[4]), .i_tready(ce_o_tready[4]),
     .o_tdata(ce_i_tdata[4]), .o_tlast(ce_i_tlast[4]), .o_tvalid(ce_i_tvalid[4]), .o_tready(ce_i_tready[4]),
     .debug(ce_debug[4]));
-  
+
+  noc_block_packet_resizer inst_noc_block_packet_resizer (
+    .bus_clk(bus_clk), .bus_rst(bus_rst),
+    .ce_clk(bus_clk), .ce_rst(bus_rst),
+    .i_tdata(ce_o_tdata[5]), .i_tlast(ce_o_tlast[5]), .i_tvalid(ce_o_tvalid[5]), .i_tready(ce_o_tready[5]),
+    .o_tdata(ce_i_tdata[5]), .o_tlast(ce_i_tlast[5]), .o_tvalid(ce_i_tvalid[5]), .o_tready(ce_i_tready[5]),
+    .debug(ce_debug[5]));
+
+  noc_block_split_stream inst_noc_block_split_stream (
+    .bus_clk(bus_clk), .bus_rst(bus_rst),
+    .ce_clk(bus_clk), .ce_rst(bus_rst),
+    .i_tdata(ce_o_tdata[6]), .i_tlast(ce_o_tlast[6]), .i_tvalid(ce_o_tvalid[6]), .i_tready(ce_o_tready[6]),
+    .o_tdata(ce_i_tdata[6]), .o_tlast(ce_i_tlast[6]), .o_tvalid(ce_i_tvalid[6]), .o_tready(ce_i_tready[6]),
+    .debug(ce_debug[6]));
+
   genvar n;
   generate
-    for (n = 5; n < NUM_CE; n = n + 1) begin
+    for (n = 7; n < NUM_CE; n = n + 1) begin
       noc_block_axi_fifo_loopback #(
         .NOC_ID(64'hF1F0_0000_0000_0000 + n),
         .STR_SINK_FIFOSIZE(11))
