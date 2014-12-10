cd8eb35f2773f9cbb5624d6ec92a20fe79046803
Matt Ettus <matt@ettus.com>
2014-12-10 10:44:21 -0800

    rfnoc: more pfb progress, just need to add coefficient setting.

--- usrp3/lib/rfnoc/pfb.v
+++ usrp3/lib/rfnoc/pfb.v
@@ -6,22 +6,23 @@ module pfb
     parameter TAPS_PER_BIN=4,
     parameter MAX_BINS_LOG2=10,
     parameter WIDTH_IN=16,
-    parameter WIDTH_OUT=16)
-
+    parameter WIDTH_OUT=16,
+    parameter PWIDTH=48,
+    parameter CLIP_BITS=4)
    (input clk, input reset, input clear,
     input set_stb, input [7:0] set_addr, input [31:0] set_data,
     input [2*WIDTH_IN-1:0] i_tdata, input i_tlast, input i_tvalid, output i_tready,
     output [2*WIDTH_IN-1:0] o_tdata, output o_tlast, output o_tvalid, input o_tready);
 
-   wire [2*WIDTH_IN-1:0] 	    delay_tdata[0:TAPS_PER_BIN];
-   wire [TAPS_PER_BIN:0] 	    delay_tlast, delay_tvalid, delay_tready;
+   wire [2*WIDTH_IN-1:0]    delay_tdata[0:TAPS_PER_BIN];
+   wire [TAPS_PER_BIN:0]    delay_tlast, delay_tvalid, delay_tready;
    
-   wire [2*PWIDTH-1:0] 		    int_result_tdata[0:TAPS_PER_BIN];
-   wire [TAPS_PER_BIN:0] 	    int_result_tlast, int_result_tvalid, int_result_tready;
-
-   wire [15:0] 			    bins;
+   wire [2*PWIDTH-1:0] 	    int_result_tdata[0:TAPS_PER_BIN];
+   wire [TAPS_PER_BIN:0]    int_result_tlast, int_result_tvalid, int_result_tready;
    
-   setting_reg #(.my_addr(BASE), .width(16)) reg_bins
+   wire [MAX_BINS_LOG2-1:0] bins;
+   
+   setting_reg #(.my_addr(BASE), .width(MAX_BINS_LOG2)) reg_bins
      (.clk(clk), .rst(reset), .strobe(set_stb), .addr(set_addr), .in(set_data),
       .out(bins), .changed());
 
@@ -40,14 +41,14 @@ module pfb
    genvar 			    i;
    generate
       for(i=0;i<TAPS_PER_BIN;i=i+1)
-	pfb_stage #(.BASE(BASE+i+1), .DWIDTH(WIDTH_IN), .CWIDTH(25), .PWIDTH(44), .MAX_BINS_LOG2(MAX_BINS_LOG2)) stage
+	pfb_stage #(.BASE(BASE+i+1), .DWIDTH(WIDTH_IN), .CWIDTH(25), .PWIDTH(PWIDTH), .MAX_BINS_LOG2(MAX_BINS_LOG2)) stage
 	    (.clk(clk), .reset(reset), .clear(clear),
 	     .set_stb(set_stb), .set_addr(set_addr), .set_data(set_data),
 	     .bins(bins),
 	     .i_tdata(delay_tdata[i]), .i_tlast(delay_tlast[i]), .i_tvalid(delay_tvalid[i]), .i_tready(delay_tready[i]),
-	     .pin_tdata(int_result_tdata[i]), .pin_tlast(int_result_tlast[i]), .pin_tvalid(int_result_tvalid[i]), .pin_tready(int_result_tready[i]),
+	     .c_tdata(int_result_tdata[i]), .c_tlast(int_result_tlast[i]), .c_tvalid(int_result_tvalid[i]), .c_tready(int_result_tready[i]),
 	     .o_tdata(delay_tdata[i+1]), .o_tlast(delay_tlast[i+1]), .o_tvalid(delay_tvalid[i+1]), .o_tready(delay_tready[i+1]),
-	     .pout_tdata(int_result_tdata[i+1]), .pout_tlast(int_result_tlast[i+1]), .pout_tvalid(int_result_tvalid[i+1]), .pout_tready(int_result_tready[i+1]));
+	     .p_tdata(int_result_tdata[i+1]), .p_tlast(int_result_tlast[i+1]), .p_tvalid(int_result_tvalid[i+1]), .p_tready(int_result_tready[i+1]));
    endgenerate
 
    // Terminate delay output.  Hopefully the last delay line will get optimized out since it is unused.
