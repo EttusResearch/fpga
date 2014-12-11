698b9acf3b7220b2bebe318835d3d8231a6375fa
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2014-12-11 12:01:08 -0800

    rfnoc: Added additional CHDR packet creation tasks and other improvements to RFNoC sim library

--- usrp3/lib/rfnoc/rfnoc_sim_lib.v
+++ usrp3/lib/rfnoc/rfnoc_sim_lib.v
@@ -2,9 +2,15 @@
 // Copyright 2014 Ettus Research LLC
 //
 
+  localparam SC16_NUM_BYTES                       = 4;
+  localparam RFNOC_CHDR_NUM_BYTES_PER_LINE        = 8;
+  localparam RFNOC_CHDR_NUM_SC16_PER_LINE         = RFNOC_CHDR_NUM_BYTES_PER_LINE/SC16_NUM_BYTES;
+
   localparam [1:0] CHDR_DATA_PKT_TYPE             = 0;
   localparam [1:0] CHDR_FC_PKT_TYPE               = 1;
   localparam [1:0] CHDR_CTRL_PKT_TYPE             = 2;
+  localparam [1:0] CHDR_EOB                       = 1 << 0;
+  localparam [1:0] CHDR_HAS_TIME                  = 1 << 1;
 
   // One register per port, spaced by 16 for 16 ports
   localparam [7:0] SR_FLOW_CTRL_CYCS_PER_ACK_BASE = 0;
@@ -17,6 +23,8 @@
   // Next destinations as allocated by the user, one per port (if used)
   localparam [7:0] SR_NEXT_DST_BASE               = 128;
   localparam [7:0] SR_READBACK_ADDR               = 255;
+  // AXI stream configuration bus (if used)
+  localparam [7:0] SR_AXI_CONFIG_BASE             = 129;
 
   reg clk;
   initial clk = 1'b0;
@@ -33,37 +41,36 @@
     #(RESET_TIME) rst = 1'b0;
   end
 
-  reg [63:0] i_tdata;
-  reg i_tlast, i_tvalid;
+  reg [63:0] i_tdata = 64'd0;
+  reg i_tlast = 1'b0, i_tvalid = 1'b0;
   wire i_tready;
   wire [63:0] o_tdata;
-  wire o_tlast, o_tvalid, o_tready;
+  wire o_tlast, o_tvalid;
+  reg o_tready = 1'b1;
 
   task SendCtrlPacket;
     input [11:0] seqnum;
     input [31:0] sid;
     input [63:0] data;
   begin
-    @(posedge clk);
     i_tdata = { 4'h8, seqnum, 16'h16, sid };
-    i_tlast = 0;
-    i_tvalid = 1;
-    while(~i_tready) @(posedge clk);
-
+    i_tlast = 1'b0;
+    i_tvalid = 1'b1;
     @(posedge clk);
+    while(~i_tready) @(posedge clk);
+    
     i_tdata = data;
-    i_tlast = 1;
+    i_tlast = 1'b1;
+    @(posedge clk);
     while(~i_tready) @(posedge clk);
-
-    @(posedge clk);
-    i_tdata = 0;
-    i_tvalid = 0;
-    i_tlast = 0;
-    @(posedge clk);
+    
+    i_tdata = 1'b0;
+    i_tvalid = 1'b0;
+    i_tlast = 1'b0;
   end
   endtask
 
-  task automatic SendDataPacket;
+  task automatic SendDummyDataPacket;
     input [1:0]  flags;
     input [11:0] seqnum;
     input [12:0] len;
@@ -73,20 +80,65 @@
   begin
     @(posedge clk);
     i_tdata = { CHDR_DATA_PKT_TYPE, flags, seqnum, (len << 3) + 16'h8, sid };
-    i_tlast = 0;
-    i_tvalid = 1;
+    i_tlast = 1'b0;
+    i_tvalid = 1'b1;
     @(posedge clk);
     while(~i_tready) @(posedge clk);
     while (i < len) begin
       i_tdata = data + i;
       i = i + 1;
-      if (i == len) i_tlast = 1;
+      if (i == len) i_tlast = 1'b1;
       @(posedge clk);
       while(~i_tready) @(posedge clk);
     end
-    i_tdata = 0;
-    i_tlast = 0;
-    i_tvalid = 0;
+    i_tdata = 1'b0;
+    i_tlast = 1'b0;
+    i_tvalid = 1'b0;
+  end
+  endtask
+  
+  task SendChdr;
+    input [1:0] pkt_type;
+    input [1:0] flags;
+    input [11:0] seqnum;
+    input [12:0] len;
+    input [31:0] sid;
+    input [63:0] vita_time;
+  begin
+    if (flags & CHDR_HAS_TIME) begin
+      i_tdata = { pkt_type, flags, seqnum, (len << 3) + 16'h16, sid };
+    end else begin
+      i_tdata = { pkt_type, flags, seqnum, (len << 3) + 16'h8, sid };
+    end
+    i_tlast = 1'b0;
+    i_tvalid = 1'b1;
+    $display("1");
     @(posedge clk);
+    while(~i_tready) @(posedge clk);
+    $display("2");
+    if (flags & CHDR_HAS_TIME) begin
+      i_tdata = vita_time;
+      @(posedge clk);
+      while(~i_tready) @(posedge clk);
+    end
+    $display("3");
+  end
+  endtask
+  
+  task SendPayload;
+    input [63:0] data;
+    input last;
+  begin
+    i_tdata = data;
+    i_tlast = last;
+    $display("4");
+    @(posedge clk);
+    while(~i_tready) @(posedge clk);
+    $display("5");
+    if (last) begin
+      i_tvalid = 1'b0;
+      i_tlast = 1'b0;
+    end
+    $display("6");
   end
   endtask
