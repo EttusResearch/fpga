b729a4617410a82f40b6ad8df14d3097261a5793
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2014-12-14 12:14:42 -0800

    rfnoc: Fixed fft shift incorrectly using blocking assignments and fixed off-by-one issue
    - Off-by-one issue resulted in the output being short by one sample sort due to a comparison with fft_size-2 instead of fft_size-1

--- usrp3/lib/rfnoc/fft_shift.v
+++ usrp3/lib/rfnoc/fft_shift.v
@@ -21,7 +21,7 @@ module fft_shift #(
   reg loading_pkt;
   reg [2:0] reconfig_stall;
   reg [$clog2(MAX_FFT_SIZE_LOG2)-1:0] fft_size_log2_latch;
-  reg [MAX_FFT_SIZE_LOG2-1:0] fft_size, fft_size_minus_2, fft_shift_mask;
+  reg [MAX_FFT_SIZE_LOG2-1:0] fft_size, fft_size_minus_1, fft_shift_mask;
   wire [WIDTH-1:0] ping_rd_data, pong_rd_data;
   reg [MAX_FFT_SIZE_LOG2-1:0] ping_rd_addr, pong_rd_addr;
   // t_user is the FFT index, this XOR is how the natural order FFT output is flipped to
@@ -37,7 +37,7 @@ module fft_shift #(
   wire pong_rd_en = ping_pong ? o_tvalid & o_tready : 1'b1;
   reg ping_loaded, pong_loaded;
   // Only fill ping (or pong) RAM if it is empty and fft size has propagated through
-  assign i_tready = (ping_pong ? ~ping_loaded        : ~pong_loaded) & ~reconfig_stall[2];
+  assign i_tready = (ping_pong ? ~ping_loaded       : ~pong_loaded) & ~reconfig_stall[2];
   reg ping_tlast, pong_tlast;
   // Dump data in ping RAM (but only if it has been loaded!) while also loading in pong RAM and vice versa
   assign o_tvalid = ping_pong ? pong_loaded         : ping_loaded;
@@ -65,39 +65,39 @@ module fft_shift #(
 
   always @(posedge clk) begin
     if (reset) begin
-      ping_pong = 1'b1;
-      ping_loaded = 1'b0;
-      pong_loaded = 1'b0;
-      ping_rd_addr = 0;
-      pong_rd_addr = 0;
-      ping_tlast = 1'b0;
-      pong_tlast = 1'b0;
-      fft_shift_mask = 0;
-      fft_size_minus_2 = 0;
-      fft_size = 0;
-      fft_size_log2_latch = 0;
-      reconfig_stall = 3'd0;
-      loading_pkt = 1'b0;
+      ping_pong             <= 1'b1;
+      ping_loaded           <= 1'b0;
+      pong_loaded           <= 1'b0;
+      ping_rd_addr          <= 0;
+      pong_rd_addr          <= 0;
+      ping_tlast            <= 1'b0;
+      pong_tlast            <= 1'b0;
+      fft_shift_mask        <= 0;
+      fft_size_minus_1      <= 0;
+      fft_size              <= 0;
+      fft_size_log2_latch   <= 0;
+      reconfig_stall        <= 3'd0;
+      loading_pkt           <= 1'b0;
     end else begin
-      fft_shift_mask = fft_size/2;
-      fft_size_minus_2 = fft_size-2;
-      fft_size = 1 << fft_size_log2_latch;
+      fft_shift_mask <= fft_size/2;
+      fft_size_minus_1 <= fft_size-1;
+      fft_size <= 1 << fft_size_log2_latch;
       // Restrict updating fft size to valid times
       // Also, deassert i_tready until updated fft size has propogated through
       if (fft_size_log2_tready & fft_size_log2_tvalid) begin
-        fft_size_log2_latch = fft_size_log2_tdata;
-        reconfig_stall = 3'b111;
+        fft_size_log2_latch <= fft_size_log2_tdata;
+        reconfig_stall <= 3'b111;
       end else begin
-        reconfig_stall[0] = 1'b0;
-        reconfig_stall[2:1] = reconfig_stall[1:0];
+        reconfig_stall[0] <= 1'b0;
+        reconfig_stall[2:1] <= reconfig_stall[1:0];
       end
       
       // Used to disable fft size configuration AXI-stream bus
       // when we are receiving a packet
       if (i_tvalid & i_tready & ~i_tlast & ~loading_pkt) begin
-        loading_pkt = 1'b1;
+        loading_pkt <= 1'b1;
       end else if (i_tvalid & i_tready & i_tlast & loading_pkt) begin
-        loading_pkt = 1'b0;
+        loading_pkt <= 1'b0;
       end
       
       // Logic to simultaneously load ping RAM and unload pong RAM. Note, write address for ping RAM handled with i_tuser, 
@@ -109,58 +109,58 @@ module fft_shift #(
           if (pong_tlast) begin
             // Special case: ping RAM loaded before pong RAM emptied
             if (ping_loaded) begin
-              ping_pong = ~ping_pong;
+              ping_pong <= ~ping_pong;
             end
-            pong_tlast = 1'b0;
-            pong_loaded = 1'b0;
-            pong_rd_addr = 0;
+            pong_tlast <= 1'b0;
+            pong_loaded <= 1'b0;
+            pong_rd_addr <= 0;
           end else begin
-            pong_rd_addr = pong_rd_addr + 1;
+            pong_rd_addr <= pong_rd_addr + 1;
           end
-          if (pong_rd_addr == fft_size_minus_2) begin
-            pong_tlast = 1'b1;
+          if (pong_rd_addr == fft_size_minus_1) begin
+            pong_tlast <= 1'b1;
           end
         end
         // Ping RAM done loading
         if (i_tvalid & i_tready & i_tlast) begin
           // Value at addr 0 already loaded (see first word fall through and avoiding a bubble state comment above)
-          ping_rd_addr = 1;
-          ping_loaded = 1'b1;
+          ping_rd_addr <= 1;
+          ping_loaded <= 1'b1;
           // We can switch to the pong RAM only if it is empty
           if (~pong_loaded) begin
-            ping_pong = ~ping_pong;
+            ping_pong <= ~ping_pong;
           end
         end
         // Special case: Ping and pong RAM loaded, wait until pong RAM unloaded.
         if (ping_loaded & ~pong_loaded) begin
-          ping_pong = ~ping_pong;
+          ping_pong <= ~ping_pong;
         end
       // Same as above, just ping / pong switched
       end else begin
         if (ping_loaded & o_tready & o_tvalid) begin
           if (ping_tlast) begin
             if (pong_loaded) begin
-              ping_pong = ~ping_pong;
+              ping_pong <= ~ping_pong;
             end
-            ping_tlast = 1'b0;
-            ping_loaded = 1'b0;
-            ping_rd_addr = 0;
+            ping_tlast <= 1'b0;
+            ping_loaded <= 1'b0;
+            ping_rd_addr <= 0;
           end else begin
-            ping_rd_addr = ping_rd_addr + 1;
+            ping_rd_addr <= ping_rd_addr + 1;
           end
-          if (ping_rd_addr == fft_size_minus_2) begin
-            ping_tlast = 1'b1;
+          if (ping_rd_addr == fft_size_minus_1) begin
+            ping_tlast <= 1'b1;
           end
         end
         if (i_tvalid & i_tready & i_tlast) begin
-          pong_rd_addr = 1;
-          pong_loaded = 1'b1;
+          pong_rd_addr <= 1;
+          pong_loaded <= 1'b1;
           if (~ping_loaded) begin
-            ping_pong = ~ping_pong;
+            ping_pong <= ~ping_pong;
           end
         end
         if (~ping_loaded & pong_loaded) begin
-          ping_pong = ~ping_pong;
+          ping_pong <= ~ping_pong;
         end
       end
     end
