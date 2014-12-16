af299d4f5582477a14379a98e5ba9f5559ec565a
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2014-12-15 16:21:20 -0800

    rfnoc: Fixed disable logic for cascade carry in for multiplier
    - PCIN in must be set to all 0s when disabled

--- usrp3/lib/rfnoc/mult_add.v
+++ usrp3/lib/rfnoc/mult_add.v
@@ -25,7 +25,7 @@ module mult_add
    wire [47:0] 		 c_tdata_int = { {DROP_TOP_P{c_tdata[WIDTH_P-1]}}, c_tdata, {(48-WIDTH_P-DROP_TOP_P){1'b0}} };
    
    wire [47:0] 		 CIN  = CASCADE_IN ? 48'hFFFF_FFFF_FFFF : c_tdata_int;
-   wire [47:0] 		 PCIN = CASCADE_IN ? c_tdata_int : 48'hFFFF_FFFF_FFFF;
+   wire [47:0] 		 PCIN = CASCADE_IN ? c_tdata_int : 48'h0000_0000_0000_0000;
 
    localparam MREG_IN = 1;    // Always have this reg
    localparam PREG_IN = (LATENCY >= 3) ? 1 : 0;
