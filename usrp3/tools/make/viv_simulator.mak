ba78fe02d41bdb0279673983b3ab2c7b1745c9ba
Ashish Chaudhari <ashish@ettus.com>
2015-02-27 15:39:20 -0800

    usrp3: Simulation infrastructure now supports Modelsim

--- usrp3/tools/make/viv_simulator.mak
+++ usrp3/tools/make/viv_simulator.mak
@@ -3,35 +3,58 @@
 #
 
 # -------------------------------------------------------------------
-# GUI Mode switch. Calling with GUI:=1 will launch Vivado GUI for build
+# Mode switches
 
+# Calling with GUI:=1 will launch Vivado GUI for build
 ifeq ($(GUI),1)
 VIVADO_MODE=gui
 else
 VIVADO_MODE=batch
 endif
+
+# Calling with FAST:=1 will switch to using unifast libs
+ifeq ($(FAST),1)
+SIM_FAST=true
+else
+SIM_FAST=false
+endif
+
 # -------------------------------------------------------------------
 
 # -------------------------------------------------------------------
 # Path variables
 
 SIMLIB_DIR = $(BASE_DIR)/../sim
+ifdef SIM_COMPLIBDIR
+COMPLIBDIR = $(SIM_COMPLIBDIR)
+endif
+
 # -------------------------------------------------------------------
 
 # Parse part name from ID
 PART_NAME=$(subst /,,$(PART_ID))
 
+# -------------------------------------------------------------------
+# Usage: SETUP_AND_LAUNCH_SIMULATION
+# Args: $1 = Simulator Name
+# -------------------------------------------------------------------
+SETUP_AND_LAUNCH_SIMULATION = \
+	@ \
+	export VIV_SIMULATOR=$1; \
+	export VIV_DESIGN_SRCS="$(DESIGN_SRCS)"; \
+	export VIV_SIM_SRCS="$(SIM_SRCS)"; \
+	export VIV_SIM_TOP=$(SIM_TOP); \
+	export VIV_PART_NAME=$(PART_NAME); \
+	export VIV_SIM_RUNTIME=$(SIM_RUNTIME_NS); \
+	export VIV_SIM_FAST="$(SIM_FAST)"; \
+	export VIV_SIM_COMPLIBDIR="$(COMPLIBDIR)"; \
+	export VIV_MODE=$(VIVADO_MODE); \
+	vivado -mode $(VIVADO_MODE) -source $(BASE_DIR)/../tools/scripts/viv_sim_project.tcl -log xsim.log -nojournal
+
 .SECONDEXPANSION:
 
 xsim:
-	@ \
-	export VIV_SIM_TOP=$(SIM_TOP); \
-	export VIV_SIM_RUNTIME=$(SIM_RUNTIME_NS); \
-	export VIV_PART_NAME=$(PART_NAME); \
-	export VIV_MODE=$(VIVADO_MODE); \
-	export VIV_DESIGN_SRCS="$(DESIGN_SRCS)"; \
-	export VIV_SIM_SRCS="$(SIM_SRCS)"; \
-	vivado -mode $(VIVADO_MODE) -source $(BASE_DIR)/../tools/scripts/viv_sim_project.tcl -log xsim.log -nojournal
+	$(call SETUP_AND_LAUNCH_SIMULATION,XSim)
 
 xclean:
 	@rm -f xsim*.log
@@ -42,7 +65,15 @@ xclean:
 	@rm -f xvlog.pb
 	@rm -f vivado_pid*.str
 
+vsim: $(COMPLIBDIR)
+	$(call SETUP_AND_LAUNCH_SIMULATION,Modelsim)
+
+vclean:
+	@rm -f modelsim*.log
+	@rm -rf modelsim_proj
+	@rm -f vivado_pid*.str
+
 # Use clean with :: to support allow "make clean" to work with multiple makefiles
-clean:: xclean
+clean:: xclean vclean
 
 .PHONY: sim clean
--- usrp3/tools/scripts/viv_sim_project.tcl
+++ usrp3/tools/scripts/viv_sim_project.tcl
@@ -5,21 +5,35 @@
 # ---------------------------------------
 # Gather all external parameters
 # ---------------------------------------
-set design_srcs $::env(VIV_DESIGN_SRCS)
-set sim_srcs    $::env(VIV_SIM_SRCS)
-set sim_top     $::env(VIV_SIM_TOP)
-set sim_runtime $::env(VIV_SIM_RUNTIME)
-set part_name   $::env(VIV_PART_NAME)
-set vivado_mode $::env(VIV_MODE)
-set working_dir [pwd]
+set simulator       $::env(VIV_SIMULATOR)
+set design_srcs     $::env(VIV_DESIGN_SRCS)
+set sim_srcs        $::env(VIV_SIM_SRCS)
+set sim_top         $::env(VIV_SIM_TOP)
+set part_name       $::env(VIV_PART_NAME)
+set sim_runtime     $::env(VIV_SIM_RUNTIME)
+set sim_fast        $::env(VIV_SIM_FAST)
+set sim_complibdir  $::env(VIV_SIM_COMPLIBDIR)
+set vivado_mode     $::env(VIV_MODE)
+set working_dir     [pwd]
 
 set sim_fileset "sim_1"
+set project_name "[string tolower $simulator]_proj"
+
+if {[expr [file isdirectory $sim_complibdir] == 0]} {
+    set sim_complibdir  ""
+    if [expr [string equal $simulator "XSim"] == 0] {
+        puts "BUILDER: \[ERROR\]: Could not resolve the location for the compiled simulation libraries."
+        puts "                  Please build libraries for chosen simulator and set the env or"
+        puts "                  makefile variable SIM_COMPLIBDIR to point to the location."
+        exit 1
+    }
+}
 
 # ---------------------------------------
 # Vivado Commands
 # ---------------------------------------
 puts "BUILDER: Creating Vivado simulation project part $part_name"
-create_project -part $part_name -force xsim_proj/xsim_proj
+create_project -part $part_name -force $project_name/$project_name
 
 foreach src_file $design_srcs {
     set src_ext [file extension $src_file ]
@@ -51,12 +65,25 @@ foreach sim_src $sim_srcs {
     add_files -fileset $sim_fileset -norecurse $sim_src
 }
 
-set_property target_simulator XSim [current_project]
+# Simulator independent config
 set_property top $sim_top [get_filesets $sim_fileset]
-set_property -name {xsim.simulate.runtime} -value "${sim_runtime}ns" -objects [get_filesets $sim_fileset]
-set_property -name {xsim.elaborate.debug_level} -value {all} -objects [get_filesets $sim_fileset]
 set_property verilog_define "SIM_RUNTIME_NS=$sim_runtime WORKING_DIR=\"$working_dir\"" [get_filesets $sim_fileset]
 
+# XSim specific settings
+set_property xsim.simulate.runtime "${sim_runtime}ns" -objects [get_filesets $sim_fileset]
+set_property xsim.elaborate.debug_level "all" -objects [get_filesets $sim_fileset]
+set_property xsim.elaborate.unifast $sim_fast -objects [get_filesets $sim_fileset]
+
+# Modelsim specific settings
+set_property compxlib.compiled_library_dir $sim_complibdir [current_project]
+set_property modelsim.simulate.runtime "${sim_runtime}ns" -objects [get_filesets $sim_fileset]
+set_property modelsim.elaborate.acc "true" -objects [get_filesets $sim_fileset]
+set_property modelsim.simulate.log_all_signals} "true" -objects [get_filesets $sim_fileset]
+set_property modelsim.simulate.vsim.more_options -value {-c} -objects [get_filesets $sim_fileset]
+set_property modelsim.elaborate.unifast $sim_fast -objects [get_filesets $sim_fileset]
+
+# Select the simulator and launch simulation
+set_property target_simulator $simulator [current_project]
 launch_simulation
 
 if [string equal $vivado_mode "batch"] {
