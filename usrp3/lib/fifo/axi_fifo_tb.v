e263e1fcdc924603946211b2d4d2463a4ad94562
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2015-06-21 16:44:08 -0700

    axi_fifo_flop: Removed combinatorial paths on AXI control signals to improve timing
    - Also updated FIFO testbench to test for correct depth, bubble states, and various read / write combinations

--- usrp3/lib/fifo/axi_fifo_tb.v
+++ usrp3/lib/fifo/axi_fifo_tb.v
@@ -1,211 +1,325 @@
 //
-// Copyright 2012-2013 Ettus Research LLC
+// Copyright 2015 Ettus Research LLC
 //
 
-
 module axi_fifo_tb();
-   
-   reg clk, reset;
-   reg read_flag, write_flag;
-   
-   reg error;
-   reg [7:0] i_tdata, o_tdata_ref;
-   wire [7:0] o_tdata;
-   reg 	      i_tvalid, o_tready;
-   wire       o_tvalid, i_tready;
-   wire [15:0] space, occupied;
 
-   always
-     #100 clk = ~clk;
+  /*********************************************
+  ** User variables
+  *********************************************/
+  localparam FIFO_SIZE = 1;
+  localparam TEST_VECTOR_SIZE = 10;
 
-   initial clk = 0;
-   
- 
-   axi_fifo 
-     #(
-       .WIDTH(8),
-       .SIZE(8)
-       )
-       dut
-	 (.clk(clk),
-	  .reset(reset),
-	  .clear(1'b0),
-	  .i_tdata(i_tdata),
-	  .i_tvalid(i_tvalid),
-	  .i_tready(i_tready),
-	  .o_tdata(o_tdata),
-	  .o_tvalid(o_tvalid),
-	  .o_tready(o_tready),    
-	  .space(space),
-	  .occupied(occupied)
-	  );
+  /*********************************************
+  ** Clocks & Reset
+  *********************************************/
+  `define CLOCK_FREQ    200e6
+  `define RESET_TIME    100
 
+  reg clk;
+  initial clk = 1'b0;
+  localparam CLOCK_PERIOD = 1e9/`CLOCK_FREQ;
+  always
+    #(CLOCK_PERIOD) clk = ~clk;
 
-   task write;
-      begin
-	 write_flag <= 1;
-	 i_tvalid <= 1'b1;	
-	 #1; 
-	 while (i_tready != 1'b1)
-	   @(posedge clk);
-	 #1;
-	 @(posedge clk);
-	 write_flag <= 0;
-	 i_tvalid <= 1'b0;
-	 i_tdata <= i_tdata + 8'h1;
+  reg reset;
+  initial begin
+    reset = 1'b1;
+    #(`RESET_TIME);
+    @(posedge clk);
+    reset = 1'b0;
+  end
+
+  /*********************************************
+  ** DUT
+  *********************************************/
+  reg [31:0] i_tdata;
+  reg i_tvalid, o_tready;
+  wire i_tready, o_tvalid;
+  wire [31:0] o_tdata;
+  reg clear;
+
+  axi_fifo #(
+    .SIZE(FIFO_SIZE),
+    .WIDTH(32))
+  dut_axi_fifo (
+    .clk(clk), .reset(reset), .clear(clear),
+    .i_tdata(i_tdata), .i_tvalid(i_tvalid), .i_tready(i_tready),
+    .o_tdata(o_tdata), .o_tvalid(o_tvalid), .o_tready(o_tready),
+    .space(), .occupied());
+
+  /*********************************************
+  ** Testbench
+  *********************************************/
+  reg [TEST_VECTOR_SIZE-1:0] i_tvalid_sequence;
+  reg [TEST_VECTOR_SIZE-1:0] o_tready_sequence;
+  integer i,k,n,i_tready_timeout;
+  reg [31:0] o_tdata_check;
+
+  initial begin
+    i_tdata = {32{1'b1}};
+    i_tvalid = 1'b0;
+    o_tready = 1'b0;
+    i_tready_timeout = 0;
+    clear = 1'b0;
+    @(negedge reset);
+    #(10*CLOCK_PERIOD)
+    @(posedge clk);
+    $display("*****************************************************");
+    $display("**              Begin Assertion Tests              **");
+    $display("*****************************************************");
+    $display("Test 1 -- Check filling FIFO");
+    // Note, if REG_OUTPUT is enabled, the FIFO has space for 1 extra entry
+    for (i = 0; i < 2**FIFO_SIZE; i = i + 1) begin
+      if (~i_tready) begin
+          $display("Test 1 FAILED!");
+          $error("FIFO size should be %d entries, but detected %d!",2**FIFO_SIZE,i);
+          $stop;
       end
-   endtask // write
-
-   task read;
-      begin
-	 read_flag <= 1;
-	 o_tready <= 1'b1;
-	 #1;
-	 while (o_tvalid != 1'b1)
-	    @(posedge clk);
-	 #1;
-	 @(posedge clk);
-	 read_flag <= 0;
-	 o_tready <= 1'b0;
-	 if (o_tdata_ref != o_tdata) begin
-	    $display("ERROR: Expected %d, got %d, at time %d",o_tdata_ref,o_tdata,$time);
-	    error <= 1'b1;
-	 end else  
-	   error <= 1'b0;
-	 o_tdata_ref = o_tdata_ref + 8'h1;
+      i_tvalid = 1'b1;
+      i_tdata = i_tdata + 32'd1;
+      @(posedge clk);
+    end
+    i_tvalid = 1'b0;
+    @(posedge clk);
+    if (i_tready) begin
+      $display("Test 1 warning!");
+      $warning("i_tready still asserted after filling FIFO with %d entries! Might be due to output registering.",i);
+      //$stop;
+    end
+    $display("Test 1 Passed!");
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+    $display("Test 2 -- Check emptying FIFO");
+    for (i = 0; i < 2**FIFO_SIZE; i = i + 1) begin
+      if (~o_tvalid) begin
+          $display("Test 2 FAILED!");
+          $error("FIFO o_tvalid not asserted! Occured at entry %d",2**FIFO_SIZE-i+1);
+          $stop;
       end
-   endtask // read
+      o_tready = 1'b1;
+      @(posedge clk);
+    end
+    o_tready = 1'b0;
+    @(posedge clk);
+    if (o_tvalid) begin
+      $display("Test 1 FAILED!");
+      $error("o_tvalid still asserted after emptying FIFO!");
+      $stop;
+    end
+    $display("Test 2 Passed!");
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+    $display("Test 3 -- Check for o_tready / i_tready dropping unexpectantly");
+    clear = 1'b1;
+    i_tdata = {32{1'b1}};
+    i_tvalid = 1'b0;
+    o_tready = 1'b0;
+    @(posedge clk);
+    clear = 1'b0;
+    @(posedge clk);
+    for (i = 0; i < 2**FIFO_SIZE-1; i = i + 1) begin
+      i_tvalid = 1'b1;
+      i_tdata = i_tdata + 32'd1;
+      @(posedge clk);
+      i_tvalid = 1'b0;
+      // Give some time to propogate
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      if (~i_tready) begin
+          $display("Test 3 FAILED!");
+          $error("i_tready deasserted unexpectantly after writing %d entries!",i+1);
+          $stop;
+      end
+      if (~o_tvalid) begin
+          $display("Test 3 FAILED!");
+          $error("o_tvalid deasserted unexpectantly after writing %d entries!",i+1);
+          $stop;
+      end
+    end
+    // Write final entry
+    i_tvalid = 1'b1;
+    i_tdata = i_tdata + 32'd1;
+    @(posedge clk);
+    i_tvalid = 1'b0;
+    @(posedge clk);
+    if (i_tready) begin
+        $display("Test 3 warning!");
+        $warning("i_tready still asserted after writing %d entries! Might be due to output registering.",i+1);
+        //$stop;
+    end
+    @(posedge clk);
+    for (i = 0; i < 2**FIFO_SIZE-1; i = i + 1) begin
+      o_tready = 1'b1;
+      @(posedge clk);
+      o_tready = 1'b0;
+      // Give some time to propogate
+      @(posedge clk);
+      @(posedge clk);
+      @(posedge clk);
+      if (~i_tready) begin
+          $display("Test 3 FAILED!");
+          $error("i_tready deasserted unexpectantly after reading %d entries!",i+1);
+          $stop;
+      end
+      if (~o_tvalid) begin
+          $display("Test 3 FAILED!");
+          $error("o_tvalid deasserted unexpectantly after reading %d entries!",i+1);
+          $stop;
+      end
+    end
+    // Read final entry
+    o_tready = 1'b1;
+    @(posedge clk);
+    o_tready = 1'b0;
+    @(posedge clk);
+    if (o_tvalid) begin
+        $display("Test 3 FAILED!");
+        $error("o_tvalid still asserted after reading %d entries!",i+1);
+        $stop;
+    end
+    @(posedge clk);
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+    $display("Test 4 -- Check for bubble states");
+    clear = 1'b1;
+    i_tdata = {32{1'b1}};
+    i_tvalid = 1'b0;
+    o_tready = 1'b0;
+    @(posedge clk);
+    clear = 1'b0;
+    @(posedge clk);
+    // Fill up half way
+    for (i = 0; i < (2**FIFO_SIZE + 1)/2; i = i + 1) begin
+      i_tvalid = 1'b1;
+      i_tdata = i_tdata + 32'd1;
+      @(posedge clk);
+    end
+    // Start reading
+    o_tready = 1'b1;
+    i_tdata = i_tdata + 32'd1;
+    @(posedge clk);
+    // Give a clock cycle for latency, but no bubbles should occur after this
+    i_tdata = i_tdata + 32'd1;
+    @(posedge clk);
+    // Continue to write and read at full rate
+    for (i = 0; i < (2**FIFO_SIZE + 1)/2; i = i + 1) begin
+      if (~i_tready) begin
+          $display("Test 4 FAILED!");
+          $error("FIFO bubble state detected when writing & reading at full rate!");
+          $stop;
+      end
+      i_tvalid = 1'b1;
+      i_tdata = i_tdata + 32'd1;
+      @(posedge clk);
+    end
+    // Read at full, write at half rate
+    for (i = 0; i < (2**FIFO_SIZE + 1)/2; i = i + 1) begin
+      if (~i_tready | ~o_tvalid) begin
+          $display("Test 4 FAILED!");
+          $error("FIFO bubble state detected when write at half rate, reading at full rate!");
+          $stop;
+      end
+      i_tvalid = ~i_tvalid;
+      if (i_tvalid) i_tdata = i_tdata + 32'd1;
+      @(posedge clk);
+    end
+    // Read at half rate, write at full rate
+    for (i = 0; i < (2**FIFO_SIZE + 1)/2; i = i + 1) begin
+      if (~i_tready | ~o_tvalid) begin
+          $display("Test 4 FAILED!");
+          $error("FIFO bubble state detected when write at half rate, reading at full rate!");
+          $stop;
+      end
+      o_tready = ~o_tready;
+      i_tvalid = 1'b1;
+      i_tdata = i_tdata + 32'd1;
+      @(posedge clk);
+    end
+    $display("Test 4 Passed!");
+    //////////////////////////////////////////////////////////////////////////////
+    // Tests combinations of i_tvalid / o_tready sequences.
+    // Test space depends on TEST_VECTOR_SIZE.
+    // Example: TEST_VECTOR_SIZE = 10 => 1024*1024 number of test sequences,
+    //          which is every possible 10 bit sequence of i_tvalid / o_tready.
+    $display("Test 5 -- Check combinations of i_tvalid / o_tready");
+    clear = 1'b1;
+    i_tdata = {32{1'b1}};
+    i_tvalid = 1'b0;
+    o_tready = 1'b0;
+    i_tready_timeout = 0;
+    i_tvalid_sequence = {TEST_VECTOR_SIZE{1'd0}};
+    o_tready_sequence = {TEST_VECTOR_SIZE{1'd0}};
+    @(posedge clk);
+    clear = 1'b0;
+    @(posedge clk);
+    for (i = 0; i < 2**TEST_VECTOR_SIZE; i = i + 1) begin
+      i_tvalid_sequence = i_tvalid_sequence + 1;
+      for (k = 0; k < 2**TEST_VECTOR_SIZE; k = k + 1) begin
+        o_tready_sequence = o_tready_sequence + 1;
+        for (n = 0; n < TEST_VECTOR_SIZE; n = n + 1) begin
+          if (o_tready_sequence[n]) begin
+            o_tready = 1'b1;
+          end else begin
+            o_tready = 1'b0;
+          end
+          // Special Case: If i_tready timed out, then i_tvalid is still asserted and we cannot
+          //               deassert i_tvalid until we see a corresponding i_tready. This is a basic
+          //               AXI stream requirement, so we will continue to assert i_tvalid regardless
+          //               of what i_tvalid_sequence would have set i_tvalid for this loop.
+          if (i_tvalid_sequence[n] | (i_tready_timeout == TEST_VECTOR_SIZE)) begin
+            i_tvalid = 1'b1;
+            if (i_tready_timeout < TEST_VECTOR_SIZE) begin
+              i_tdata = i_tdata + 32'd1;
+            end
+            @(posedge clk);
+            i_tready_timeout = 0;
+            // Wait for i_tready until timeout. Timeouts may occur when o_tready_sequence
+            // has o_tready not asserted for several clock cycles.
+            while(~i_tready & (i_tready_timeout < TEST_VECTOR_SIZE)) begin
+              @(posedge clk)
+              i_tready_timeout = i_tready_timeout + 1;
+            end
+          end else begin
+            i_tvalid = 1'b0;
+            @(posedge clk);
+          end
+        end
+      end
+      // Reset starting conditions for the test sequences
+      clear = 1'b1;
+      i_tdata = {32{1'b1}};
+      i_tvalid = 1'b0;
+      o_tready = 1'b0;
+      i_tready_timeout = 0;
+      @(posedge clk);
+      clear = 1'b0;
+      @(posedge clk);
+    end
+    $display("Test 5 Passed!");
+    $display("All tests PASSED!");
+    $stop;
+  end
 
-   initial
-     begin
-	reset <= 1'b0;
-	error <= 1'b0;
-	i_tdata <= 8'b00;
-	o_tdata_ref <= 8'b00;
-	i_tvalid <= 1'b0;
-	o_tready <= 1'b0;
-	read_flag <= 0;
-	write_flag <= 0;
-	
-	repeat(10) @(posedge clk);
-	reset <= 1'b1;
-   	repeat(10) @(posedge clk);
-	reset <= 1'b0;
-	@(posedge clk);
-	@(negedge clk);
+  // Check the input counting sequence independent of
+  // i_tvalid / o_tready sequences.
+  always @(posedge clk) begin
+    if (reset) begin
+      o_tdata_check <= 32'd0;
+    end else begin
+      if (clear) begin
+        o_tdata_check <= 32'd0;
+      end
+      if (o_tready & o_tvalid) begin
+        o_tdata_check <= o_tdata_check + 32'd1;
+        if (o_tdata != o_tdata_check) begin
+          $display("Test FAILED!");
+          $error("Incorrect output!");
+          $stop;
+        end
+      end
+    end
+  end
 
-	// FIFO Should be empty now, check avail space
-	if (space != 16'd256)
-	  begin $display("ERROR: FIFO is empty, space should read 256 not %d at time %d",space,$time); error <= 1; end
-	if (occupied != 16'd0)
-	  begin $display("ERROR: FIFO is empty, occupied should read 0 not %d at time %d",occupied,$time); error <= 1; end
-	if (o_tvalid == 1'b1)
-	  begin $display("ERROR: FIFO is empty, o_tvalid should be 0 at time %d",$time); error <= 1; end
-	@(posedge clk);
-	// Push 1 item onto FIFO, check fullness updates accordingly
-	write();
-	@(posedge clk);
-	@(negedge clk);
-	if (space != 16'd255)
-	  begin $display("ERROR: FIFO space should read 255 not %d at time %d",space,$time); error <= 1; end
-	if (occupied != 16'd1)
-	  begin $display("ERROR: FIFO occupied should read 1 not %d at time %d",occupied,$time); error <= 1; end
-	if (o_tvalid == 1'b0)
-	  begin $display("ERROR: FIFO is not empty, o_tvalid should be 1 at time %d",$time); error <= 1; end
-	// Pop FIFO once, check it goes back empty OK.
-	@(posedge clk);	
-	read();
-	@(posedge clk);
-	@(negedge clk);
-	if (space != 16'd256)
-	  begin $display("ERROR: FIFO is empty, space should read 256 not %d at time %d",space,$time); error <= 1; end
-	if (occupied != 16'd0)
-	  begin $display("ERROR: FIFO is empty, occupied should read 0 not %d at time %d",occupied,$time); error <= 1; end
-	if (o_tvalid == 1'b1)
-	  begin $display("ERROR: FIFO is empty, o_tvalid should be 0 at time %d",$time); error <= 1; end
-	// Push FIFO 255 times and see if it goes full incorrectly
-	repeat(255) begin
-	   @(posedge clk);
-	   write();
-	end
-	@(posedge clk);
-	@(negedge clk);
-	if (space != 16'd1)
-	  begin $display("ERROR: FIFO is nearly full, space should read 1 not %d at time %d",space,$time); error <= 1; end
-	if (occupied != 16'd255)
-	  begin $display("ERROR: FIFO is nearly full, occupied should read 255 not %d at time %d",occupied,$time); error <= 1; end
-	if (o_tvalid == 1'b0)
-	  begin $display("ERROR: FIFO is nearly full, o_tvalid should be 1 at time %d",$time); error <= 1; end
-	if (i_tready == 1'b0)
-	  begin $display("ERROR: FIFO is nearly full, i_tready should be 1 at time %d",$time); error <= 1; end
-	// Push FIFO one more time, now it should be full
-	@(posedge clk);	
-	write();
-	@(posedge clk);
-	@(negedge clk);
-	if (space != 16'd0)
-	  begin $display("ERROR: FIFO is full, space should read 0 not %d at time %d",space,$time); error <= 1; end
-	if (occupied != 16'd256)
-	  begin $display("ERROR: FIFO is full, occupied should read 256 not %d at time %d",occupied,$time); error <= 1; end
-	if (o_tvalid == 1'b0)
-	  begin $display("ERROR: FIFO is full, o_tvalid should be 1 at time %d",$time); error <= 1; end
-	if (i_tready == 1'b1)
-	  begin $display("ERROR: FIFO is full, i_tready should be 0 at time %d",$time); error <= 1; end
-	// POP FIFO once, check it went nonfull.
-	@(posedge clk);	
-	read();
-	@(posedge clk);
-	@(negedge clk);
-	if (space != 16'd1)
-	  begin $display("ERROR: FIFO is nearly full, space should read 1 not %d at time %d",space,$time); error <= 1; end
-	if (occupied != 16'd255)
-	  begin $display("ERROR: FIFO is nearly full, occupied should read 255 not %d at time %d",occupied,$time); error <= 1; end
-	if (o_tvalid == 1'b0)
-	  begin $display("ERROR: FIFO is nearly full, o_tvalid should be 1 at time %d",$time); error <= 1; end
-	if (i_tready == 1'b0)
-	  begin $display("ERROR: FIFO is nearly full, i_tready should be 1 at time %d",$time); error <= 1; end
-	// Take FIFO to empty state
-	repeat(255) begin
-	   @(posedge clk);
-	   read();
-	end
-	@(posedge clk);
-	@(negedge clk);
-	if (space != 16'd256)
-	  begin $display("ERROR: FIFO is empty, space should read 256 not %d at time %d",space,$time); error <= 1; end
-	if (occupied != 16'd0)
-	  begin $display("ERROR: FIFO is empty, occupied should read 0 not %d at time %d",occupied,$time); error <= 1; end
-	if (o_tvalid == 1'b1)
-	  begin $display("ERROR: FIFO is empty, o_tvalid should be 0 at time %d",$time); error <= 1; end
-	// Push 1 item onto FIFO
-	@(posedge clk);
-	write();
-	@(posedge clk);
-	// Now write twice as fast as we read, and write 256 times, which should leave, 129 elements in FIFO.
-	fork
-	   repeat(256) begin
-	      write();
-	      @(posedge clk);
-	   end
-	   repeat(128) begin
-	      read();
-	      @(posedge clk);
-	      @(posedge clk);
-	   end
-	join
-	@(posedge clk);
-	if (space != 16'd127)
-	  begin $display("ERROR: FIFO space should read 127 not %d at time %d",space,$time); error <= 1; end
-	if (occupied != 16'd129)
-	  begin $display("ERROR: FIFO occupied should read 129 not %d at time %d",occupied,$time); error <= 1; end
-	
-
-	
-	//
-	// END
-	//
-	repeat(10) @(posedge clk);
-	$finish;
-     end // initial begin
-   
-endmodule // axi_fifo_tb
+endmodule
