3cf6857dc7d2d602e0e71fdc7e853786172b82b8
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2015-08-10 13:44:23 -0700

    lib: Reverted erroneous change to axi settings reg

--- usrp3/lib/control/axi_setting_reg.v
+++ usrp3/lib/control/axi_setting_reg.v
@@ -10,7 +10,7 @@ module axi_setting_reg #(
   parameter DATA_AT_RESET = 0,
   parameter VALID_AT_RESET = 0,
   parameter REPEATS = 0,
-  parameter REVERSE_BITS = 0
+  parameter MSB_ALIGN = 0
 )
 (
   input clk, input reset, 
@@ -18,15 +18,7 @@ module axi_setting_reg #(
   output reg [WIDTH-1:0] o_tdata, output o_tlast, output reg o_tvalid, input o_tready
 );
 
-  wire [WIDTH-1:0] set_data_reversed;
   reg init;
-
-  genvar i;
-  generate
-    for (i = 0; i < WIDTH; i = i + 1) begin
-      assign set_data_reversed[WIDTH-1-i] = set_data[i];
-    end
-  endgenerate
 
   always @(posedge clk) begin
     if (reset) begin
@@ -36,7 +28,7 @@ module axi_setting_reg #(
     end else begin
       if (set_stb & (ADDR==set_addr)) begin
         init <= 1'b1;
-        o_tdata  <= (REVERSE_BITS == 0) ? set_data[WIDTH-1:0] : set_data_reversed;
+        o_tdata  <= (MSB_ALIGN == 0) ? set_data[WIDTH-1:0] : set_data[31:32-WIDTH];
         o_tvalid <= 1'b1;
       end
       if (o_tvalid & o_tready) begin
