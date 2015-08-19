452d36ef6e9c2f9a7e38c8e5e20b07361788f84c
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2015-08-19 10:25:42 -0700

    rfnoc: phase_accum: Phase increment stream no longer throttles output

--- usrp3/lib/rfnoc/phase_accum.v
+++ usrp3/lib/rfnoc/phase_accum.v
@@ -22,7 +22,7 @@ module phase_accum #(
   wire output_round_tvalid, output_round_tready, output_round_tlast;
 
   // Reset accumulator output on i_tlast
-  wire [WIDTH_ACCUM-1:0] output_tdata = i_tlast ? {WIDTH_ACCUM{1'b0}} : accum;
+  wire [WIDTH_ACCUM-1:0] output_tdata = (i_tvalid & i_tlast & i_tready) ? {WIDTH_ACCUM{1'b0}} : accum;
 
   // Phase accumulator, can rotate in either direction
   always @(posedge clk) begin
@@ -30,21 +30,21 @@ module phase_accum #(
       accum      <= 'd0;
       accum_next <= 'd0;
       phase_inc  <= 'd0;
-    end else if (i_tvalid & i_tready) begin
-      if (i_tlast) begin
+    end else if (output_round_tready) begin
+      if (i_tvalid & i_tlast) begin
         accum       <= {WIDTH_ACCUM{1'b0}};
         accum_next  <= $signed(i_tdata);
         phase_inc   <= $signed(i_tdata);
       end else begin
         if (accum >= POS_ROLLOVER) begin
-            accum_next <= accum_next + phase_inc - 2*POS_ROLLOVER;
-            accum      <= accum + phase_inc - 2*POS_ROLLOVER;
+          accum_next <= accum_next + phase_inc - 2*POS_ROLLOVER;
+          accum      <= accum + phase_inc - 2*POS_ROLLOVER;
         end else if (accum <= NEG_ROLLOVER) begin
-            accum_next <= accum_next + phase_inc - 2*NEG_ROLLOVER;
-            accum      <= accum + phase_inc - 2*NEG_ROLLOVER;
+          accum_next <= accum_next + phase_inc - 2*NEG_ROLLOVER;
+          accum      <= accum + phase_inc - 2*NEG_ROLLOVER;
         end else begin
-            accum_next <= accum_next + phase_inc;
-            accum      <= accum + phase_inc;
+          accum_next <= accum_next + phase_inc;
+          accum      <= accum + phase_inc;
         end
       end
     end
@@ -54,7 +54,7 @@ module phase_accum #(
     // Bypass rounding if accumulator width is same as output width
     if (WIDTH_ACCUM == WIDTH_OUT) begin
       assign output_round_tdata = output_tdata;
-      assign output_round_tvalid = i_tvalid;
+      assign output_round_tvalid = 1'b1;
       assign output_round_tlast = i_tlast;
       assign i_tready = output_round_tready;
     end else begin
@@ -63,7 +63,7 @@ module phase_accum #(
         .WIDTH_OUT(WIDTH_OUT))
       axi_round (
         .clk(clk), .reset(reset),
-        .i_tdata(output_tdata), .i_tlast(i_tlast), .i_tvalid(i_tvalid), .i_tready(i_tready),
+        .i_tdata(output_tdata), .i_tlast(i_tlast), .i_tvalid(1'b1), .i_tready(i_tready),
         .o_tdata(output_round_tdata), .o_tlast(output_round_tlast), .o_tvalid(output_round_tvalid), .o_tready(output_round_tready));
     end
   endgenerate
