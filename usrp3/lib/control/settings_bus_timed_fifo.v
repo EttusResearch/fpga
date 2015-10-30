c207b1753c1d11bab83d7b93e87e147220639414
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2015-10-29 17:10:38 -0700

    lib: Added address masking to settings bus timed FIFO to prevent
    - Prevents filling FIFO with commands not intended to be timed

--- usrp3/lib/control/settings_bus_timed_fifo.v
+++ usrp3/lib/control/settings_bus_timed_fifo.v
@@ -4,11 +4,13 @@
 // Stores settings bus transaction in a FIFO and releases them based on VITA time input
 
 module settings_bus_timed_fifo #(
-  parameter AWIDTH=8,
-  parameter DWIDTH=32,
-  parameter SIZE=6)
+  parameter BASE   = 0,
+  parameter RANGE  = 256,
+  parameter AWIDTH = 8,
+  parameter DWIDTH = 32,
+  parameter SIZE   = 6)
 (
-  input clk, input rst,
+  input clk, input reset,
   input [63:0] vita_time,
   input set_stb, input [AWIDTH-1:0] set_addr, input [DWIDTH-1:0] set_data, input [63:0] set_time,
   output set_stb_timed, output [AWIDTH-1:0] set_addr_timed, output [DWIDTH-1:0] set_data_timed, input ready
@@ -19,14 +21,18 @@ module settings_bus_timed_fifo #(
   wire [63:0] set_time_fifo;
   reg [63:0] vita_time_reg;
 
+  // Prevent writes to FIFO if they are outside the valid address range. This prevents timed commands
+  // not intended for this address range from blocking the FIFO.
+  wire set_stb_masked = (set_addr >= BASE && set_addr <= BASE+RANGE-1) ? set_stb : 1'b0;
+
   axi_fifo #(.WIDTH(AWIDTH+DWIDTH+64),.SIZE(SIZE)) axi_fifo (
-    .clk(ce_clk), .reset(rst), .clear(1'b0),
-    .i_tdata({set_addr,set_data,set_time}), .i_tvalid(set_stb), .i_tready(),
+    .clk(clk), .reset(reset), .clear(1'b0),
+    .i_tdata({set_addr,set_data,set_time}), .i_tvalid(set_stb_masked), .i_tready(),
     .o_tdata({set_addr_timed,set_data_timed,set_time_fifo}), .o_tvalid(o_tvalid), .o_tready(o_tready),
     .space(), .occupied());
 
   always @(posedge clk) begin
-    if (rst) begin
+    if (reset) begin
       o_tready      <= 1'b0;
       vita_time_reg <= 64'd0;
     end else begin
