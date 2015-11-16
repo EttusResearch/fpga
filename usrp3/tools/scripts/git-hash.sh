3a5e6c7e931f6fc1ea77217d1759f0c7f8e3c0c2
Ashish Chaudhari <ashish@ettus.com>
2015-11-15 20:42:52 -0800

    tools: Minor script cleanup
    - Made git-hash.sh work on systems with no git installed
    - Deleted viv_check_timing.py

--- usrp3/tools/scripts/git-hash.sh
+++ usrp3/tools/scripts/git-hash.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if (git diff --quiet); then
-#Clean
-    short_hash="0$(git rev-parse --verify HEAD --short)"
+if [[ $(command -v git) = "" ]]; then
+    short_hash="FFFFFFFF"
 else
-#Dirty
-    short_hash="F$(git rev-parse --verify HEAD --short)"
+    if (git diff --quiet); then
+        #Clean
+        short_hash="0$(git rev-parse --verify HEAD --short)"
+    else
+        #Dirty
+        short_hash="F$(git rev-parse --verify HEAD --short)"
+    fi
 fi
-
-echo $short_hash
-
+echo ${short_hash^^}
