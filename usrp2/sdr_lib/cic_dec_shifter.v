675279d24c0e645f7000ae129947b6fe7c553b6a
Ryan Volz <rvolz@mit.edu>
2015-12-08 15:53:55 -0500

    usrp2: Include variable bit gain set by register after CIC filter.
    Decimation by N creates a possible log2(N)/2 bits of additional
    precision. Currently, this extra precision is lost in the DDC chain by
    internal conversions down to 17-18 bits from 24 bits for multiplication.
    This patch inserts a variable bit gain after the CIC filter and before
    any of the decimation precision is lost, allowing that precision to be
    kept at the expense of higher order bits. When the signal levels are low
    enough so that the higher order bits are not used, this is a great
    tradeoff to make.
    
    Using this requires a change to the UHD host code to specify the bit
    gain.
    
    This commit adds variable bitgain with clipping as a separate module
    (variable_part_select_and_clip) that is called from the cic decimator.
    The cic_dec_shifter module was modified so that a multiplexer is
    inferred, reducing its footprint. The cic decimator pipeline was also
    fixed so that the strobe matches the earliest valid output, and an
    additional pipeline stage was added for the variable bitgain in order to
    meet timing requirements.

--- usrp2/sdr_lib/cic_dec_shifter.v
+++ usrp2/sdr_lib/cic_dec_shifter.v
@@ -23,13 +23,14 @@
 // NOTE   This only works for N=4, max decim rate of 128
 // NOTE   signal "rate" is EQUAL TO the actual rate, no more -1 BS
 
-module cic_dec_shifter(rate,signal_in,signal_out);
-   parameter bw = 16;
-   parameter maxbitgain = 28;
-
-   input [7:0] rate;
-   input       wire [bw+maxbitgain-1:0] signal_in;
-   output      reg [bw-1:0] signal_out;
+module cic_dec_shifter
+   #(parameter bw = 16, maxbitgain = 28)
+     (input clk,
+      input [7:0] rate,
+      input strobe_in,
+      output reg strobe_out,
+      input [bw+maxbitgain-1:0] signal_in,
+      output reg [bw-1:0] signal_out);
 
    function [4:0] bitgain;
       input [7:0] rate;
@@ -43,7 +44,7 @@ module cic_dec_shifter(rate,signal_in,signal_out);
 	8'd32 : bitgain = 20;
 	8'd64 : bitgain = 24;
 	8'd128 : bitgain = 28;
-	
+
 	// Nearest without overflow -- ceil(N*log2(rate))
 	8'd3 : bitgain = 7;
 	8'd5 : bitgain = 10;
@@ -67,40 +68,18 @@ module cic_dec_shifter(rate,signal_in,signal_out);
 	default : bitgain = 28;
       endcase // case(rate)
    endfunction // bitgain
-   
-   wire [4:0] 	  shift = bitgain(rate);
-   
-   // We should be able to do this, but can't ....
-   // assign 	  signal_out = signal_in[shift+bw-1:shift];
-   
-   always @*
-     case(shift)
-       5'd0  : signal_out = signal_in[0+bw-1:0];
-       5'd4  : signal_out = signal_in[4+bw-1:4];
-       5'd7  : signal_out = signal_in[7+bw-1:7];
-       5'd8  : signal_out = signal_in[8+bw-1:8];
-       5'd10 : signal_out = signal_in[10+bw-1:10];
-       5'd11 : signal_out = signal_in[11+bw-1:11];
-       5'd12 : signal_out = signal_in[12+bw-1:12];
-       5'd13 : signal_out = signal_in[13+bw-1:13];
-       5'd14 : signal_out = signal_in[14+bw-1:14];
-       5'd15 : signal_out = signal_in[15+bw-1:15];
-       5'd16 : signal_out = signal_in[16+bw-1:16];
-       5'd17 : signal_out = signal_in[17+bw-1:17];
-       5'd18 : signal_out = signal_in[18+bw-1:18];
-       5'd19 : signal_out = signal_in[19+bw-1:19];
-       5'd20 : signal_out = signal_in[20+bw-1:20];
-       5'd21 : signal_out = signal_in[21+bw-1:21];
-       5'd22 : signal_out = signal_in[22+bw-1:22];
-       5'd23 : signal_out = signal_in[23+bw-1:23];
-       5'd24 : signal_out = signal_in[24+bw-1:24];
-       5'd25 : signal_out = signal_in[25+bw-1:25];
-       5'd26 : signal_out = signal_in[26+bw-1:26];
-       5'd27 : signal_out = signal_in[27+bw-1:27];
-       5'd28 : signal_out = signal_in[28+bw-1:28];
-       
-       default : signal_out = signal_in[28+bw-1:28];
-     endcase // case(shift)
+
+   // use register for shift to limit delay
+   // force user encoding so signal_out can be inferred as a multiplexer
+   (* signal_encoding = "user" *)
+   reg [4:0] shift;
+   always @(posedge clk)
+     shift <= bitgain(rate);
+
+   always @(posedge clk) begin
+     signal_out <= signal_in[shift +: bw];
+     strobe_out <= strobe_in;
+   end
 
 endmodule // cic_dec_shifter
 
