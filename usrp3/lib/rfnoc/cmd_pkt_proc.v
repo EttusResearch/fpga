4cd70998f858b0b5c19938745b4140df7eeef081
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2016-01-29 09:06:09 -0800

    rfnoc: cmd_pkt_proc: Fixed issue with not handling tlast with timed commands properly

--- usrp3/lib/rfnoc/cmd_pkt_proc.v
+++ usrp3/lib/rfnoc/cmd_pkt_proc.v
@@ -1,192 +1,222 @@
 //
-// Copyright 2014 Ettus Research LLC
+// Copyright 2016 Ettus Research LLC
 //
 
 // Command Packet Processor
-//  Accepts compressed vita extension context packets of the following form:
+//  Accepts compressed vita packets of the following form:
 //       { VITA Compressed Header, Stream ID }
 //       { Optional 64 bit time }
-//       { 16'h0, setting bus address [15:0], setting [31:0] }
+//       { 24'h0, settings bus address [7:0], data [31:0] }
 //
-//  If there is a timestamp, packet is held until that time comes.
-//  Goes immediately if there is no timestamp or if time has passed.
-//  Sends out setting to setting bus, and then generates a response packet
-//  with the same sequence number, the src/dest swapped streamid, and the actual time
+//  Sends out setting to setting bus and generates a response packet
+//  with the same sequence number, the src/dst stream ID swapped, and the actual time
 //  the setting was sent.
-//
-// Note -- if t0 is the requested time, the actual send time on the setting bus is t0 + 1 cycle.
-// Note 2 -- if t1 is the actual time the setting bus, t1+2 is the reported time.
 
 module cmd_pkt_proc #(
-   parameter NUM_SR_BUSES = 1)
-  (input clk, input reset, input clear,
+  parameter AWIDTH = 8,
+  parameter DWIDTH = 32,
+  parameter NUM_SR_BUSES = 1, // One per block port
+  parameter FIFO_SIZE = 5     // Depth of command FIFO
+)(
+  input clk, input reset, input clear,
 
-   input [63:0] cmd_tdata, input cmd_tlast, input cmd_tvalid, output reg cmd_tready,
-   output reg [63:0] resp_tdata, output reg resp_tlast, output resp_tvalid, input resp_tready,
+  input [63:0] cmd_tdata, input cmd_tlast, input cmd_tvalid, output cmd_tready,
+  (* mark_debug = "true", dont_touch = "true" *) output reg [63:0] resp_tdata, (* mark_debug = "true", dont_touch = "true" *) output reg resp_tlast, (* mark_debug = "true", dont_touch = "true" *) output resp_tvalid, (* mark_debug = "true", dont_touch = "true" *) input resp_tready,
 
-   input [63:0] vita_time,
+  input [63:0] vita_time,
 
-   output [NUM_SR_BUSES-1:0] set_stb, output [7:0] set_addr, output [31:0] set_data, output reg [63:0] set_time,
-   input ready,
+  output [NUM_SR_BUSES-1:0] set_stb, output [AWIDTH-1:0] set_addr, output [DWIDTH-1:0] set_data, output reg [63:0] set_time,
+  input ready,
 
-   input [NUM_SR_BUSES*64-1:0] readback);
+  input [NUM_SR_BUSES*64-1:0] readback
+);
 
-   localparam RC_HEAD   = 4'd0;
-   localparam RC_TIME   = 4'd1;
-   localparam RC_DATA   = 4'd2;
-   localparam RC_DUMP   = 4'd3;
-   localparam RC_RESP_WAIT = 4'd4;
-   localparam RC_RESP_HEAD = 4'd5;
-   localparam RC_RESP_TIME = 4'd6;
-   localparam RC_RESP_DATA = 4'd7;
+  // Input FIFO
+  wire [63:0] cmd_fifo_tdata;
+  wire cmd_fifo_tlast, cmd_fifo_tvalid;
+  reg cmd_fifo_tready;
+  axi_fifo #(.WIDTH(65), .SIZE(FIFO_SIZE)) axi_fifo (
+    .clk(clk), .reset(reset), .clear(clear),
+    .i_tdata({cmd_tlast,cmd_tdata}), .i_tvalid(cmd_tvalid), .i_tready(cmd_tready),
+    .o_tdata({cmd_fifo_tlast,cmd_fifo_tdata}), .o_tvalid(cmd_fifo_tvalid), .o_tready(cmd_fifo_tready),
+    .space(), .occupied());
 
-   wire 	 IS_EC = cmd_tdata[63:62] == 2'b10;
-   wire 	 HAS_TIME = cmd_tdata[61];
-   reg 		 HAS_TIME_reg;
+  localparam RC_HEAD      = 4'd0;
+  localparam RC_TIME      = 4'd1;
+  localparam RC_DATA      = 4'd2;
+  localparam RC_DUMP      = 4'd3;
+  localparam RC_RESP_WAIT = 4'd4;
+  localparam RC_RESP_HEAD = 4'd5;
+  localparam RC_RESP_TIME = 4'd6;
+  localparam RC_RESP_DATA = 4'd7;
 
-   reg [3:0] 	 rc_state;
+  wire IS_CMD_PKT = cmd_fifo_tdata[63:62] == 2'b10;
+  wire HAS_TIME   = cmd_fifo_tdata[61];
+  reg HAS_TIME_reg;
 
-   reg [11:0] seqnum_reg;
-   reg [31:0] sid_reg;
-   reg [15:0] src_sid_reg, dst_sid_reg;
-   reg [3:0] block_port_reg;
-   wire [11:0] seqnum    = cmd_tdata[59:48];
-   wire [31:0] sid       = cmd_tdata[31:0];
-   wire [15:0] src_sid   = sid[31:16];
-   wire [15:0] dst_sid   = sid[15:0];
-   wire [3:0] block_port = sid[3:0];
-   reg [NUM_SR_BUSES-1:0] block_port_stb;
-   integer k;
+  (* mark_debug = "true", dont_touch = "true" *) reg [3:0] rc_state;
 
-   always @(posedge clk)
-     if(reset)
-       begin
-	  rc_state <= RC_HEAD;
-	  HAS_TIME_reg <= 1'b0;
-	  sid_reg <= 'd0;
-	  seqnum_reg <= 'd0;
-	  set_time <= 'd0;
-	  src_sid_reg <= 'd0;
-	  dst_sid_reg <= 'd0;
-	  block_port_reg <= 'd0;
-	  block_port_stb <= 'd0;
-       end
-     else
-	 case(rc_state)
-	   RC_HEAD :
-	     if(cmd_tvalid)
-	       begin
-		  src_sid_reg <= src_sid;
-		  dst_sid_reg <= dst_sid;
-		  block_port_reg <= block_port;
-		  // Set strobe for addressed block port's settings register bus
-		  for (k = 0; k < NUM_SR_BUSES; k = k + 1) begin
-		    if (block_port == k) begin
-		      block_port_stb[k] <= 1'b1;
-		    end else begin
-		      block_port_stb[k] <= 1'b0;
-		    end
-		  end
-		  seqnum_reg <= seqnum;
-		  HAS_TIME_reg <= HAS_TIME;
-		  if(IS_EC)
-		    if(HAS_TIME) begin
-		      rc_state <= RC_TIME;
-		    end else begin
-		      set_time <= 64'd0;
-		      rc_state <= RC_DATA;
-		    end
-		  else
-		    if(~cmd_tlast)
-		      rc_state <= RC_DUMP;
-	       end
+  reg [11:0] seqnum_reg;
+  reg [31:0] sid_reg;
+  reg [15:0] src_sid_reg, dst_sid_reg;
+  reg [3:0] block_port_reg;
+  wire [11:0] seqnum    = cmd_fifo_tdata[59:48];
+  wire [31:0] sid       = cmd_fifo_tdata[31:0];
+  wire [15:0] src_sid   = sid[31:16];
+  wire [15:0] dst_sid   = sid[15:0];
+  wire [3:0] block_port = sid[3:0];
+  reg [NUM_SR_BUSES-1:0] block_port_stb;
+  integer k;
 
-	   RC_TIME :
-	     if(cmd_tvalid) begin
-	       set_time <= cmd_tdata;
-	       if(cmd_tlast)
-		 rc_state <= RC_RESP_WAIT;
-	       else
-		 rc_state <= RC_DATA;
-		 end
+  always @(posedge clk) begin
+    if (reset) begin
+      rc_state       <= RC_HEAD;
+      HAS_TIME_reg   <= 1'b0;
+      sid_reg        <= 'd0;
+      seqnum_reg     <= 'd0;
+      set_time       <= 'd0;
+      src_sid_reg    <= 'd0;
+      dst_sid_reg    <= 'd0;
+      block_port_reg <= 'd0;
+      block_port_stb <= 'd0;
+    end else begin
+      case (rc_state)
+        RC_HEAD : begin
+          if (cmd_fifo_tvalid) begin
+            src_sid_reg <= src_sid;
+            dst_sid_reg <= dst_sid;
+            block_port_reg <= block_port;
+            // Set strobe for addressed block port's settings register bus
+            for (k = 0; k < NUM_SR_BUSES; k = k + 1) begin
+              if (block_port == k) begin
+                block_port_stb[k] <= 1'b1;
+              end else begin
+                block_port_stb[k] <= 1'b0;
+              end
+            end
+            seqnum_reg <= seqnum;
+            HAS_TIME_reg <= HAS_TIME;
+            if (IS_CMD_PKT) begin
+              if (HAS_TIME) begin
+                rc_state <= RC_TIME;
+              end else begin
+                set_time <= 64'd0;
+                rc_state <= RC_DATA;
+              end
+            end else begin
+              if (~cmd_fifo_tlast) begin
+                rc_state <= RC_DUMP;
+              end
+            end
+          end
+        end
 
-	   RC_DATA :
-	     if(cmd_tvalid)
-	       if(ready)
-		 if(cmd_tlast)
-		   rc_state <= RC_RESP_WAIT;
-		 else
-		   rc_state <= RC_DUMP;
+        RC_TIME : begin
+          if (cmd_fifo_tvalid) begin
+            set_time <= cmd_fifo_tdata;
+            if (cmd_fifo_tlast) begin
+              rc_state <= RC_RESP_WAIT;
+            end else begin
+              rc_state <= RC_DATA;
+            end
+          end
+        end
 
-	   RC_DUMP :
-	     if(cmd_tvalid)
-	       if(cmd_tlast)
-		 rc_state <= RC_RESP_WAIT;
+        RC_DATA : begin
+          if (cmd_fifo_tvalid) begin
+            if (ready) begin
+              if (cmd_fifo_tlast) begin
+                rc_state <= RC_RESP_WAIT;
+              // Long command packet support
+              end else begin
+                rc_state <= RC_DATA;
+              end
+            end
+          end
+        end
 
-	   // Wait a clock cycle to ensure readback
-	   // has time to propagate
-	   RC_RESP_WAIT :
-	     rc_state <= RC_RESP_HEAD;
+        // This should never happen
+        RC_DUMP : begin
+          if (cmd_fifo_tvalid) begin
+            if (cmd_fifo_tlast) begin
+              rc_state <= RC_HEAD;
+            end
+          end
+        end
 
-	   RC_RESP_HEAD :
-	     if(resp_tready)
-	       rc_state <= RC_RESP_TIME;
+        // Wait a clock cycle to ensure readback
+        // has time to propagate
+        RC_RESP_WAIT : begin
+          rc_state <= RC_RESP_HEAD;
+        end
 
-	   RC_RESP_TIME :
-	     if(resp_tready)
-	       rc_state <= RC_RESP_DATA;
+        RC_RESP_HEAD : begin
+          if (resp_tready) begin
+            rc_state <= RC_RESP_TIME;
+          end
+        end
 
-	   RC_RESP_DATA:
-	     if(resp_tready)
-	       rc_state <= RC_HEAD;
-	   
-	   default :
-	     rc_state <= RC_HEAD;
-	 endcase // case (rc_state)
+        RC_RESP_TIME : begin
+          if (resp_tready) begin
+            rc_state <= RC_RESP_DATA;
+          end
+        end
 
-   always @*
-     case (rc_state)
-       RC_HEAD : cmd_tready <= 1'b1;
-       RC_TIME : cmd_tready <= cmd_tlast;
-       RC_DATA : cmd_tready <= ready;
-       RC_DUMP : cmd_tready <= 1'b1;
-       default : cmd_tready <= 1'b0;
-     endcase // case (rc_state)
+        RC_RESP_DATA : begin
+          if (resp_tready) begin
+            rc_state <= RC_HEAD;
+          end
+        end
 
-   reg [63:0] readback_reg;
-   wire [63:0] readback_mux[0:NUM_SR_BUSES-1];
-   always @(posedge clk) begin
-     if (reset | clear) begin
-       readback_reg   <= 64'd0;
-     end else begin
-       if (block_port_reg > NUM_SR_BUSES-1) begin
-         readback_reg <= 64'd0;
-       end else begin
-         readback_reg <= readback_mux[block_port_reg];
-       end
-     end
-   end
+        default : rc_state <= RC_HEAD;
+      endcase // case (rc_state)
+    end
+  end
 
-   genvar i;
-   generate
-     for (i = 0; i < NUM_SR_BUSES; i = i + 1) begin
-       assign set_stb[i] = (rc_state == RC_DATA) & ready & cmd_tvalid & block_port_stb[i];
-       assign readback_mux[i] = readback[64*i+63:64*i];
-     end
-   endgenerate
+  always @* begin
+    case (rc_state)
+      RC_HEAD : cmd_fifo_tready <= 1'b1;
+      RC_TIME : cmd_fifo_tready <= 1'b1;
+      RC_DATA : cmd_fifo_tready <= ready;
+      RC_DUMP : cmd_fifo_tready <= 1'b1;
+      default : cmd_fifo_tready <= 1'b0;
+    endcase // case (rc_state)
+  end
 
-   assign set_addr = cmd_tdata[39:32];
-   assign set_data = cmd_tdata[31:0];
+  reg [63:0] readback_reg;
+  wire [63:0] readback_mux[0:NUM_SR_BUSES-1];
+  always @(posedge clk) begin
+    if (reset | clear) begin
+      readback_reg   <= 64'd0;
+    end else begin
+      if (block_port_reg > NUM_SR_BUSES-1) begin
+        readback_reg <= 64'd0;
+      end else begin
+        readback_reg <= readback_mux[block_port_reg];
+      end
+    end
+  end
 
-   always @*
-     case (rc_state)
-       RC_RESP_HEAD : { resp_tlast, resp_tdata } <= {1'b0, 4'hE, seqnum_reg, 16'd24, dst_sid_reg, src_sid_reg};
-       RC_RESP_TIME : { resp_tlast, resp_tdata } <= {1'b0, vita_time};
-       RC_RESP_DATA : { resp_tlast, resp_tdata } <= {1'b1, readback_reg};
-       default : { resp_tlast, resp_tdata } <= 65'h0;
-     endcase // case (rc_state)
+  genvar i;
+  generate
+    for (i = 0; i < NUM_SR_BUSES; i = i + 1) begin
+      assign set_stb[i] = (rc_state == RC_DATA) & ready & cmd_fifo_tvalid & block_port_stb[i];
+      assign readback_mux[i] = readback[64*i+63:64*i];
+    end
+  endgenerate
 
-   assign resp_tvalid = (rc_state == RC_RESP_HEAD) | (rc_state == RC_RESP_TIME) | (rc_state == RC_RESP_DATA);
-   
+  assign set_addr = cmd_fifo_tdata[AWIDTH-1+32:32];
+  assign set_data = cmd_fifo_tdata[DWIDTH-1:0];
+
+  always @* begin
+    case (rc_state)
+      RC_RESP_HEAD : { resp_tlast, resp_tdata } <= {1'b0, 4'hE, seqnum_reg, 16'd24, dst_sid_reg, src_sid_reg};
+      RC_RESP_TIME : { resp_tlast, resp_tdata } <= {1'b0, vita_time};
+      RC_RESP_DATA : { resp_tlast, resp_tdata } <= {1'b1, readback_reg};
+      default : { resp_tlast, resp_tdata } <= 65'h0;
+    endcase
+  end
+
+  assign resp_tvalid = (rc_state == RC_RESP_HEAD) | (rc_state == RC_RESP_TIME) | (rc_state == RC_RESP_DATA);
+
 endmodule // radio_ctrl_proc
-
