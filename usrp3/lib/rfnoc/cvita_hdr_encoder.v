1290c63cd6bca243b9c9f12aaf1c145a4e3373cc
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2016-03-17 03:41:40 -0700

    rfnoc: cvita_hdr_encoder: Fix module name

--- usrp3/lib/rfnoc/cvita_hdr_encoder.v
+++ usrp3/lib/rfnoc/cvita_hdr_encoder.v
@@ -3,7 +3,7 @@
 //
 // Encodes CVITA packet header fields into a header word
 
-module cvita_hdr_decoder (
+module cvita_hdr_encoder (
   input [1:0] pkt_type, input eob, input has_time,
   input [11:0] seqnum, input [15:0] pkt_len, input [31:0] sid,
   input [63:0] vita_time,
