551636848ac11c05e53099b1f50814063fbac522
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2016-04-04 18:10:44 -0700

    lib: sim: Fixed AXI FIFO 2clk simulation model to properly handle SIZE = 0
    - Replaces with SIZE = 1 instead

--- usrp3/lib/sim/fifo/axi_fifo_2clk_sim.v
+++ usrp3/lib/sim/fifo/axi_fifo_2clk_sim.v
@@ -18,6 +18,8 @@ module axi_fifo_2clk #(
     input o_tready
 );
 
+    localparam FIFOSIZE = (SIZE < 1) ? 1 : SIZE;
+
     // Synchronizers
     wire o_rst_sync, i_rst_sync;
     synchronizer #(
@@ -36,45 +38,45 @@ module axi_fifo_2clk #(
         .in(reset), .out(o_rst_sync));
 
     // Gray counter encode / decode + synchronizers
-    reg [SIZE-1:0] wr_addr, rd_addr;
-    wire [SIZE-1:0] wr_addr_sync, rd_addr_sync;
-    wire [SIZE-1:0] wr_addr_gray_sync, wr_addr_gray, rd_addr_gray_sync, rd_addr_gray;
+    reg [FIFOSIZE-1:0] wr_addr, rd_addr;
+    wire [FIFOSIZE-1:0] wr_addr_sync, rd_addr_sync;
+    wire [FIFOSIZE-1:0] wr_addr_gray_sync, wr_addr_gray, rd_addr_gray_sync, rd_addr_gray;
     synchronizer #(
         .INITIAL_VAL(0),
-        .WIDTH(SIZE),
+        .WIDTH(FIFOSIZE),
         .STAGES(SYNC_STAGES))
     synchronizer_rd_addr_gray (
         .clk(i_aclk), .rst(o_rst_sync),
         .in(rd_addr_gray), .out(rd_addr_gray_sync));
     synchronizer #(
         .INITIAL_VAL(0),
-        .WIDTH(SIZE),
+        .WIDTH(FIFOSIZE),
         .STAGES(SYNC_STAGES))
     synchronizer_wr_addr_gray (
         .clk(o_aclk), .rst(i_rst_sync),
         .in(wr_addr_gray), .out(wr_addr_gray_sync));
-    bin2gray #(.WIDTH(SIZE))
+    bin2gray #(.WIDTH(FIFOSIZE))
     bin2gray_wr_addr (.bin(wr_addr), .gray(wr_addr_gray));
-    bin2gray #(.WIDTH(SIZE))
+    bin2gray #(.WIDTH(FIFOSIZE))
     bin2gray_rd_addr (.bin(rd_addr), .gray(rd_addr_gray));
-    gray2bin #(.WIDTH(SIZE))
+    gray2bin #(.WIDTH(FIFOSIZE))
     gray2bin_wr_addr (.gray(wr_addr_gray_sync), .bin(wr_addr_sync));
-    gray2bin #(.WIDTH(SIZE))
+    gray2bin #(.WIDTH(FIFOSIZE))
     gray2bin_rd_addr (.gray(rd_addr_gray_sync), .bin(rd_addr_sync));
 
-    reg [SIZE:0] i_occupied;
-    reg [SIZE:0] i_space;
+    reg [FIFOSIZE:0] i_occupied;
+    reg [FIFOSIZE:0] i_space;
     reg i_full;
     reg i_empty;
-    reg [SIZE:0] o_occupied;
-    reg [SIZE:0] o_space;
+    reg [FIFOSIZE:0] o_occupied;
+    reg [FIFOSIZE:0] o_space;
     reg o_full;
     reg o_empty;
 
-    reg [WIDTH:0] mem[0:2**(SIZE)-1];
+    reg [WIDTH:0] mem[0:2**(FIFOSIZE)-1];
     integer i;
     initial begin
-        for (i = 0; i < 1 << SIZE; i = i + 1) begin
+        for (i = 0; i < 1 << FIFOSIZE; i = i + 1) begin
             mem[i] = 'd0;
         end
     end
@@ -98,7 +100,7 @@ module axi_fifo_2clk #(
             i_full     <= 1'b0;
             i_empty    <= 1'b1;
             i_occupied <= 'd0;
-            i_space    <= (1'b1 << SIZE);
+            i_space    <= (1'b1 << FIFOSIZE);
         end else begin
             if ((rd_addr_sync-1'b1 == wr_addr) & i_tvalid & i_tready) begin
                 i_tready <= 1'b0;
@@ -118,33 +120,33 @@ module axi_fifo_2clk #(
             if (i_tvalid) begin
                 if (wr_addr == rd_addr_sync) begin
                     if (i_full) begin
-                        i_occupied <= 1'b1 << SIZE;
+                        i_occupied <= 1'b1 << FIFOSIZE;
                         i_space    <= 'd0;
                     end else begin
                         i_occupied <= 'd1;
-                        i_space    <= (1'b1 << SIZE)-1'b1;
+                        i_space    <= (1'b1 << FIFOSIZE)-1'b1;
                     end
                 end else if (wr_addr > rd_addr_sync) begin
                     i_occupied <= wr_addr - rd_addr_sync + 1'b1;
-                    i_space    <= (1'b1 << SIZE) - (wr_addr - rd_addr_sync + 1'b1);
+                    i_space    <= (1'b1 << FIFOSIZE) - (wr_addr - rd_addr_sync + 1'b1);
                 end else begin
-                    i_occupied <= wr_addr+1'b1 + (1'b1 << SIZE)-1'b1 - rd_addr_sync + 1'b1;
+                    i_occupied <= wr_addr+1'b1 + (1'b1 << FIFOSIZE)-1'b1 - rd_addr_sync + 1'b1;
                     i_space    <= rd_addr_sync - wr_addr - 1'b1;
                 end
             end else begin
                 if (wr_addr == rd_addr_sync) begin
                     if (i_full) begin
-                        i_occupied <= 1'b1 << SIZE;
+                        i_occupied <= 1'b1 << FIFOSIZE;
                         i_space    <= 'd0;
                     end else begin
                         i_occupied <= 'd0;
-                        i_space    <= 1'b1 << SIZE;
+                        i_space    <= 1'b1 << FIFOSIZE;
                     end
                 end else if (wr_addr > rd_addr_sync) begin
                     i_occupied <= wr_addr - rd_addr_sync;
-                    i_space    <= (1'b1 << SIZE) - (wr_addr - rd_addr_sync);
+                    i_space    <= (1'b1 << FIFOSIZE) - (wr_addr - rd_addr_sync);
                 end else begin
-                    i_occupied <= wr_addr+1'b1 + (1'b1 << SIZE)-1'b1 - rd_addr_sync;
+                    i_occupied <= wr_addr+1'b1 + (1'b1 << FIFOSIZE)-1'b1 - rd_addr_sync;
                     i_space    <= rd_addr_sync - wr_addr;
                 end
             end
@@ -188,33 +190,33 @@ module axi_fifo_2clk #(
             if (o_tready) begin
                 if (wr_addr_sync == rd_addr) begin
                     if (~o_empty) begin
-                        o_occupied <= (1'b1 << SIZE) - 1'b1;
+                        o_occupied <= (1'b1 << FIFOSIZE) - 1'b1;
                         o_space    <= 'd1;
                     end else begin
                         o_occupied <= 'd0;
-                        o_space    <= (1'b1 << SIZE);
+                        o_space    <= (1'b1 << FIFOSIZE);
                     end
                 end else if (wr_addr_sync > rd_addr) begin
                     o_occupied <= wr_addr_sync - rd_addr - 1'b1;
-                    o_space    <= (1'b1 << SIZE) - (wr_addr_sync - rd_addr - 1'b1);
+                    o_space    <= (1'b1 << FIFOSIZE) - (wr_addr_sync - rd_addr - 1'b1);
                 end else begin
-                    o_occupied <= wr_addr_sync+1'b1 + (1'b1 << SIZE)-1'b1 - rd_addr - 1'b1;
+                    o_occupied <= wr_addr_sync+1'b1 + (1'b1 << FIFOSIZE)-1'b1 - rd_addr - 1'b1;
                     o_space    <= rd_addr - wr_addr_sync + 1'b1;
                 end
             end else begin
                 if (wr_addr_sync == rd_addr) begin
                     if (~o_empty) begin
-                        o_occupied <= 1'b1 << SIZE;
+                        o_occupied <= 1'b1 << FIFOSIZE;
                         o_space    <= 'd0;
                     end else begin
                         o_occupied <= 'd0;
-                        o_space    <= 1'b1 << SIZE;
+                        o_space    <= 1'b1 << FIFOSIZE;
                     end
                 end else if (wr_addr_sync > rd_addr) begin
                     o_occupied <= wr_addr_sync - rd_addr;
-                    o_space    <= (1'b1 << SIZE) - (wr_addr_sync - rd_addr);
+                    o_space    <= (1'b1 << FIFOSIZE) - (wr_addr_sync - rd_addr);
                 end else begin
-                    o_occupied <= wr_addr_sync+1'b1 + (1'b1 << SIZE)-1'b1 - rd_addr;
+                    o_occupied <= wr_addr_sync+1'b1 + (1'b1 << FIFOSIZE)-1'b1 - rd_addr;
                     o_space    <= rd_addr - wr_addr_sync;
                 end
             end
