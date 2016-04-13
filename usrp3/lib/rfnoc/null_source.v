26369e9947ac70182163c70148d6a0f757112180
Ashish Chaudhari <ashish@ettus.com>
2016-04-13 14:37:06 -0700

    null_src_sink: Fixed multiple minor startup isses

--- usrp3/lib/rfnoc/null_source.v
+++ usrp3/lib/rfnoc/null_source.v
@@ -13,24 +13,23 @@ module null_source
     input set_stb, input [7:0] set_addr, input [31:0] set_data,
     output [63:0] o_tdata, output o_tlast, output o_tvalid, input o_tready);
 
-   reg [11:0] 	  seqnum;
-   wire [15:0] 	  rate;
-   reg [1:0] 	  state;
-   reg [15:0] 	  line_number;
+   reg [11:0]      seqnum;
+   wire [15:0]      rate;
+   reg [1:0]      state;
+   reg [15:0]      line_number;
    
-   wire [63:0] 	  int_tdata;
-   wire 	  int_tlast, int_tvalid, int_tready;
+   wire [63:0]      int_tdata;
+   wire      int_tlast, int_tvalid, int_tready;
 
-   wire [15:0] 	  len;
-   reg [15:0] 	  count;
-   reg [15:0] 	  packet_count;
-   wire 	  changed_sid;
-   wire 	  enable;
+   wire [15:0]      len;
+   reg [15:0]      count;
+   reg [15:0]      packet_count;
+   wire      enable;
    
    setting_reg #(.my_addr(SR_LINES_PER_PACKET), .width(16)) len_reg
      (.clk(clk), .rst(reset), .strobe(set_stb), .addr(set_addr), .in(set_data),
       .out(len), .changed());
-		 
+
    setting_reg #(.my_addr(SR_LINE_RATE), .width(16)) rate_reg
      (.clk(clk), .rst(reset), .strobe(set_stb), .addr(set_addr), .in(set_data),
       .out(rate), .changed());
@@ -44,41 +43,34 @@ module null_source
    localparam DATA = 2'd2;
 
    always @(posedge clk)
-     if(reset | clear)
-       begin
-	  state <= IDLE;
-	  count <= 0;
-       end
-     else
-       begin
-	  if(changed_sid)
-	    seqnum <= 0;
-	  case(state)
-	    IDLE :
-	      if(enable)
-		state <= HEAD;
-	    HEAD :
-	      if(int_tvalid & int_tready)
-		begin
-		   count <= 1;
-		   state <= DATA;
-		   seqnum <= seqnum + 1;
-		end
-	    DATA :
-	      if(int_tvalid & int_tready)
-		if(count >= len)
-		  begin
-		     state <= IDLE;
-		     count <= 0;
-		  end
-		else
-		  count <= count + 1;
-	    default :
-	      state <= IDLE;
-	  endcase // case (state)
-       end // else: !if(reset)
-   
-   
+      if(reset | clear) begin
+         state <= IDLE;
+         count <= 0;
+         seqnum <= 0;
+      end else begin
+         case(state)
+         IDLE :
+            if(enable)
+               state <= HEAD;
+         HEAD :
+            if(int_tvalid & int_tready) begin
+               count <= 1;
+               state <= DATA;
+               seqnum <= seqnum + 1;
+            end
+         DATA :
+            if(int_tvalid & int_tready)
+               if(count >= len) begin
+                  state <= IDLE;
+                  count <= 0;
+               end
+            else
+               count <= count + 1;
+         default :
+            state <= IDLE;
+         endcase // case (state)
+      end // else: !if(reset)
+
    wire [15:0] pkt_len = { len[12:0], 3'b000 } + 16'd8;
    
    assign int_tdata = (state == HEAD) ? { 4'b0000, seqnum, pkt_len, sid } : {~count,count,count,count} ;
@@ -86,16 +78,16 @@ module null_source
 
    reg [15:0]  line_timer;
    always @(posedge clk)
-     if(reset | clear)
-       line_timer <= 0;
-     else
-       if(line_timer == 0)
-	 line_timer <= rate;
-       else
-	 line_timer <= line_timer - 1;
-   
+      if(reset | clear)
+         line_timer <= 0;
+      else
+         if(line_timer == 0)
+            line_timer <= rate;
+         else
+            line_timer <= line_timer - 1;
+
    assign int_tvalid = ((state==HEAD)|(state==DATA)) & (line_timer==0);
-   
+
    axi_packet_gate #(.WIDTH(64), .SIZE(10)) gate
      (.clk(clk), .reset(reset), .clear(clear),
       .i_tdata(int_tdata), .i_tlast(int_tlast), .i_terror(1'b0), .i_tvalid(int_tvalid), .i_tready(int_tready),
