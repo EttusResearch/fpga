14ffc925f4d948e240e2288f8eac86ec79be57a0
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2016-05-27 19:18:58 -0500

    rfnoc: axi_drop_packet, axi_drop_partial_packet: Fixed broken functionality when max packet size equals 1

--- usrp3/lib/rfnoc/axi_drop_packet.v
+++ usrp3/lib/rfnoc/axi_drop_packet.v
@@ -27,94 +27,105 @@ module axi_drop_packet #(
   input o_tready
 );
 
-  reg [WIDTH-1:0] int_tdata = {WIDTH{1'b0}};
-  reg  int_tlast = 1'b0, int_tvalid;
-  wire int_tready;
-
-  reg [$clog2(MAX_PKT_SIZE)-1:0] wr_addr, prev_wr_addr, rd_addr;
-  reg [$clog2(MAX_PKT_SIZE):0] in_pkt_cnt, out_pkt_cnt; // Extra bit to for wrap around
-  reg full, empty;
-
-  reg [WIDTH:0] mem[0:2**($clog2(MAX_PKT_SIZE))-1];
-  // Initialize RAM to all zeros
-  integer i;
-  initial begin
-    for (i = 0; i < (1 << $clog2(MAX_PKT_SIZE)); i = i + 1) begin
-      mem[i] = 'd0;
-    end
-  end
-
-  // Write logic
-  always @(posedge clk) begin
-    if (reset | clear) begin
-      wr_addr       <= 'd0;
-      prev_wr_addr  <= 'd0;
-      in_pkt_cnt    <= 'd0;
-      full          <= 1'b0;
+  generate
+    // Packet size of 1 is efficiently implemented as a pass through with i_terror masking i_tvalid.
+    if (MAX_PKT_SIZE == 1) begin
+      assign o_tdata  = i_tdata;
+      assign o_tlast  = i_tlast;
+      assign o_tvalid = i_tvalid & ~i_terror;
+      assign i_tready = o_tready;
+    // All other packet sizes
     end else begin
-      if (i_tvalid & i_tready) begin
-        mem[wr_addr] <= {i_tdata,i_tlast};
-        wr_addr      <= wr_addr + 1'b1;
+      reg [WIDTH-1:0] int_tdata = {WIDTH{1'b0}};
+      reg  int_tlast = 1'b0, int_tvalid;
+      wire int_tready;
+
+      reg [$clog2(MAX_PKT_SIZE)-1:0] wr_addr, prev_wr_addr, rd_addr;
+      reg [$clog2(MAX_PKT_SIZE)-1:0] in_pkt_cnt, out_pkt_cnt;
+      reg full, empty;
+
+      reg [WIDTH:0] mem[0:2**($clog2(MAX_PKT_SIZE))-1];
+      // Initialize RAM to all zeros
+      integer i;
+      initial begin
+        for (i = 0; i < (1 << $clog2(MAX_PKT_SIZE)); i = i + 1) begin
+          mem[i] = 'd0;
+        end
       end
-      if (~full & (wr_addr == rd_addr-1'b1) & i_tvalid & i_tready) begin
-        full         <= 1'b1;
-      end
-      if (full & int_tvalid & int_tready) begin
-        full         <= 1'b0;
-      end
-      // Rewind logic
-      if (i_tvalid & i_tready & i_tlast) begin
-        if (i_terror) begin
-          wr_addr      <= prev_wr_addr;
+
+      // Write logic
+      always @(posedge clk) begin
+        if (reset | clear) begin
+          wr_addr       <= 'd0;
+          prev_wr_addr  <= 'd0;
+          in_pkt_cnt    <= 'd0;
+          full          <= 1'b0;
         end else begin
-          in_pkt_cnt   <= in_pkt_cnt + 1'b1;
-          prev_wr_addr <= wr_addr + 1'b1;
+          if (i_tvalid & i_tready) begin
+            mem[wr_addr] <= {i_tdata,i_tlast};
+            wr_addr      <= wr_addr + 1'b1;
+          end
+          if (~full & (wr_addr == rd_addr-1'b1) & i_tvalid & i_tready) begin
+            full         <= 1'b1;
+          end
+          if (full & int_tvalid & int_tready) begin
+            full         <= 1'b0;
+          end
+          // Rewind logic
+          if (i_tvalid & i_tready & i_tlast) begin
+            if (i_terror) begin
+              wr_addr      <= prev_wr_addr;
+            end else begin
+              in_pkt_cnt   <= in_pkt_cnt + 1'b1;
+              prev_wr_addr <= wr_addr + 1'b1;
+            end
+          end
         end
       end
-    end
-  end
 
-  assign i_tready = ~full;
+      assign i_tready = ~full;
 
-  // Read logic
-  wire hold = (in_pkt_cnt == out_pkt_cnt);
+      // Read logic
+      wire hold = (in_pkt_cnt == out_pkt_cnt);
 
-  always @(posedge clk) begin
-    if (reset | clear) begin
-      rd_addr     <= 'd0;
-      out_pkt_cnt <= 'd0;
-      empty       <= 1'b1;
-      int_tvalid  <= 1'b0;
-    end else begin
-      if (~int_tvalid | int_tready) begin
-        {int_tdata,int_tlast} <= mem[rd_addr];
-        int_tvalid            <= ~empty & ~hold;
-      end
-      if ((~empty & ~hold) & (~int_tvalid | int_tready)) begin
-        rd_addr      <= rd_addr + 1;
-      end
-      if (empty & i_tvalid & i_tready) begin
-        empty        <= 1'b0;
-      end
-      if ((~empty & ~hold) & (wr_addr-1'b1 == rd_addr) & (~int_tvalid | int_tready)) begin
-        if (~i_tvalid) begin
-          empty      <= 1'b1;
+      always @(posedge clk) begin
+        if (reset | clear) begin
+          rd_addr     <= 'd0;
+          out_pkt_cnt <= 'd0;
+          empty       <= 1'b1;
+          int_tvalid  <= 1'b0;
+        end else begin
+          if (~int_tvalid | int_tready) begin
+            {int_tdata,int_tlast} <= mem[rd_addr];
+            int_tvalid            <= ~empty & ~hold;
+          end
+          if ((~empty & ~hold) & (~int_tvalid | int_tready)) begin
+            rd_addr      <= rd_addr + 1;
+          end
+          if (empty & i_tvalid & i_tready) begin
+            empty        <= 1'b0;
+          end
+          if ((~empty & ~hold) & (wr_addr-1'b1 == rd_addr) & (~int_tvalid | int_tready)) begin
+            if (~i_tvalid) begin
+              empty      <= 1'b1;
+            end
+          end
+          // Prevent output until we have a full packet
+          if (int_tvalid & int_tready & int_tlast) begin
+            out_pkt_cnt  <= out_pkt_cnt + 1'b1;
+          end
         end
       end
-      // Prevent output until we have a full packet
-      if (int_tvalid & int_tready & int_tlast) begin
-        out_pkt_cnt  <= out_pkt_cnt + 1'b1;
-      end
-    end
-  end
 
-  // Output register stage
-  // Added specifically to prevent Vivado synth from putting block RAM
-  // output register in a slice instead of using the one block RAM primitive
-  axi_fifo_flop2 #(.WIDTH(WIDTH+1)) axi_fifo_flop2 (
-    .clk(clk), .reset(reset), .clear(clear),
-    .i_tdata({int_tlast,int_tdata}), .i_tvalid(int_tvalid), .i_tready(int_tready),
-    .o_tdata({o_tlast,o_tdata}), .o_tvalid(o_tvalid), .o_tready(o_tready),
-    .space(), .occupied());
+      // Output register stage
+      // Added specifically to prevent Vivado synth from putting block RAM
+      // output register in a slice instead of using the one block RAM primitive
+      axi_fifo_flop2 #(.WIDTH(WIDTH+1)) axi_fifo_flop2 (
+        .clk(clk), .reset(reset), .clear(clear),
+        .i_tdata({int_tlast,int_tdata}), .i_tvalid(int_tvalid), .i_tready(int_tready),
+        .o_tdata({o_tlast,o_tdata}), .o_tvalid(o_tvalid), .o_tready(o_tready),
+        .space(), .occupied());
+    end
+  endgenerate
 
 endmodule
--- usrp3/lib/rfnoc/axi_drop_partial_packet.v
+++ usrp3/lib/rfnoc/axi_drop_partial_packet.v
@@ -19,109 +19,119 @@ module axi_drop_partial_packet #(
   output [WIDTH-1:0] o_tdata, output o_tlast, output o_tvalid, input o_tready
 );
 
-  // Settings register
-  wire [$clog2(MAX_PKT_SIZE+1)-1:0] pkt_size_sr;
-  setting_reg #(.my_addr(SR_PKT_SIZE_ADDR), .width($clog2(MAX_PKT_SIZE+1)), .at_reset(SR_PKT_SIZE_DEFAULT)) sr_n (
-    .clk(clk), .rst(reset | SR_PKT_SIZE_FIXED), .strobe(set_stb), .addr(set_addr), .in(set_data),
-    .out(pkt_size_sr), .changed());
-
-  // Do not change n unless block is not active
-  reg active;
-  reg [$clog2(MAX_PKT_SIZE+1)-1:0] pkt_size = SR_PKT_SIZE_FIXED;
-  always @(posedge clk) begin
-    if (reset | clear) begin
-      active <= 1'b0;
-    end else begin
-      if (i_tready & i_tvalid) begin
-        active <= 1'b1;
-      end
-    end
-    if (clear | ~active) begin
-      pkt_size <= (pkt_size_sr == 0) ? 1 : pkt_size_sr;
-    end
-  end
-
-  wire [WIDTH-1:0] int_tdata;
-  wire int_tlast, int_tvalid, int_tready;
-  wire i_tlast_int, i_terror;
-
-  reg small_pkt, large_pkt;
-  wire hold_last_sample;
-  reg release_due_to_error;
-  reg [$clog2(MAX_PKT_SIZE+1)-1:0] in_cnt;
-  reg [15:0] in_pkt_cnt, out_pkt_cnt;
-  always @(posedge clk) begin
-    if (reset | clear) begin
-      small_pkt             <= 1'b0;
-      large_pkt             <= 1'b0;
-      release_due_to_error  <= 1'b0;
-      in_cnt                <= 1;
-      in_pkt_cnt            <= 0;
-      out_pkt_cnt           <= 0;
-    end else begin
-      if (i_tvalid & i_tready) begin
-        if (in_cnt == pkt_size | i_tlast_int) begin
-          in_cnt     <= 1;
-        end else begin
-          in_cnt     <= in_cnt + 1;
-        end
-      end
-      if (pkt_size == 1) begin
-        small_pkt      <= 1'b0;
-        large_pkt      <= 1'b0;
-      end else begin
-        if (i_tvalid & i_tready) begin
-          if ((in_cnt == pkt_size-1'b1) & ~i_tlast) begin
-            small_pkt  <= 1'b0;
-          end else begin
-            small_pkt  <= 1'b1;
-          end
-          if ((in_cnt == pkt_size) & ~i_tlast) begin
-            large_pkt  <= 1'b1;
-          end
-          if (large_pkt) begin
-            large_pkt  <= 1'b0;
-          end
-        end
-      end
-      if (i_tvalid & i_tready & i_tlast) begin
-        in_pkt_cnt     <= in_pkt_cnt + 1'b1;
-      end
-      if (int_tvalid & int_tready & int_tlast & ~hold_last_sample) begin
-        out_pkt_cnt    <= out_pkt_cnt + 1'b1;
-      end
-      if (((i_tvalid & i_tready & i_terror) | flush) & (in_pkt_cnt != out_pkt_cnt)) begin
-        release_due_to_error <= 1'b1;
-      end
-      if (release_due_to_error & int_tvalid & int_tready & int_tlast & (in_pkt_cnt == out_pkt_cnt-1'b1)) begin
-        release_due_to_error <= 1'b0;
-      end
-    end
-  end
-
-  assign hold_last_sample = ((in_pkt_cnt == out_pkt_cnt) | (in_pkt_cnt == out_pkt_cnt+1)) & ~release_due_to_error & (pkt_size != 1);
-
-  assign i_tlast_int = i_tlast | large_pkt;
-  assign i_terror    = i_tlast & i_tvalid & (small_pkt | large_pkt);
-
-  // FIFO with ability to rewind write pointer back if input packet is flagged as bad
-  axi_drop_packet #(.WIDTH(WIDTH+1), .MAX_PKT_SIZE(MAX_PKT_SIZE)) axi_drop_packet (
-    .clk(clk), .reset(reset), .clear(clear),
-    .i_tdata({i_tlast,i_tdata}), .i_tvalid(i_tvalid), .i_tlast(i_tlast_int), .i_terror(i_terror), .i_tready(i_tready),
-    .o_tdata({int_tlast,int_tdata}), .o_tvalid(int_tvalid), .o_tlast(), .o_tready(int_tready & ~(hold_last_sample & int_tlast)));
-
   generate
-    if (HOLD_LAST_WORD) begin
-      axi_fifo_flop2 #(.WIDTH(WIDTH+1)) axi_fifo_flop2 (
-        .clk(clk), .reset(reset), .clear(clear),
-        .i_tdata({int_tlast,int_tdata}), .i_tvalid(int_tvalid & ~(hold_last_sample & int_tlast)), .i_tready(int_tready),
-        .o_tdata({o_tlast,o_tdata}), .o_tvalid(o_tvalid), .o_tready(o_tready),
-        .space(), .occupied());
+    // Packet size of 1 means it is impossible to form a partial packet, so this module does nothing...
+    if (MAX_PKT_SIZE == 1) begin
+      assign o_tdata  = i_tdata;
+      assign o_tlast  = i_tlast;
+      assign o_tvalid = i_tvalid;
+      assign i_tready = o_tready;
+    // All other packet sizes
     end else begin
-      assign o_tdata  = int_tdata;
-      assign o_tlast  = int_tlast;
-      assign o_tvalid = int_tvalid;
-      assign o_tready = int_tready;
+      // Settings register
+      wire [$clog2(MAX_PKT_SIZE+1)-1:0] pkt_size_sr;
+      setting_reg #(.my_addr(SR_PKT_SIZE_ADDR), .width($clog2(MAX_PKT_SIZE+1)), .at_reset(SR_PKT_SIZE_DEFAULT)) sr_n (
+        .clk(clk), .rst(reset | SR_PKT_SIZE_FIXED), .strobe(set_stb), .addr(set_addr), .in(set_data),
+        .out(pkt_size_sr), .changed());
+
+      // Do not change n unless block is not active
+      reg active;
+      reg [$clog2(MAX_PKT_SIZE+1)-1:0] pkt_size = SR_PKT_SIZE_FIXED;
+      always @(posedge clk) begin
+        if (reset | clear) begin
+          active <= 1'b0;
+        end else begin
+          if (i_tready & i_tvalid) begin
+            active <= 1'b1;
+          end
+        end
+        if (clear | ~active) begin
+          pkt_size <= (pkt_size_sr == 1) ? 1 : pkt_size_sr;
+        end
+      end
+
+      wire [WIDTH-1:0] int_tdata;
+      wire int_tlast, int_tvalid, int_tready;
+      wire i_tlast_int, i_terror;
+
+      reg small_pkt, large_pkt;
+      wire hold_last_sample;
+      reg release_due_to_error;
+      reg [$clog2(MAX_PKT_SIZE+1)-1:0] in_cnt;
+      reg [15:0] in_pkt_cnt, out_pkt_cnt;
+      always @(posedge clk) begin
+        if (reset | clear) begin
+          small_pkt             <= 1'b0;
+          large_pkt             <= 1'b0;
+          release_due_to_error  <= 1'b0;
+          in_cnt                <= 1;
+          in_pkt_cnt            <= 0;
+          out_pkt_cnt           <= 0;
+        end else begin
+          if (i_tvalid & i_tready) begin
+            if (in_cnt == pkt_size | i_tlast_int) begin
+              in_cnt     <= 1;
+            end else begin
+              in_cnt     <= in_cnt + 1;
+            end
+          end
+          if (pkt_size == 1) begin
+            small_pkt      <= 1'b0;
+            large_pkt      <= 1'b0;
+          end else begin
+            if (i_tvalid & i_tready) begin
+              if ((in_cnt == pkt_size-1'b1) & ~i_tlast) begin
+                small_pkt  <= 1'b0;
+              end else begin
+                small_pkt  <= 1'b1;
+              end
+              if ((in_cnt == pkt_size) & ~i_tlast) begin
+                large_pkt  <= 1'b1;
+              end
+              if (large_pkt) begin
+                large_pkt  <= 1'b0;
+              end
+            end
+          end
+          if (i_tvalid & i_tready & i_tlast) begin
+            in_pkt_cnt     <= in_pkt_cnt + 1'b1;
+          end
+          if (int_tvalid & int_tready & int_tlast & ~hold_last_sample) begin
+            out_pkt_cnt    <= out_pkt_cnt + 1'b1;
+          end
+          if (((i_tvalid & i_tready & i_terror) | flush) & (in_pkt_cnt != out_pkt_cnt)) begin
+            release_due_to_error <= 1'b1;
+          end
+          if (release_due_to_error & int_tvalid & int_tready & int_tlast & (in_pkt_cnt == out_pkt_cnt-1'b1)) begin
+            release_due_to_error <= 1'b0;
+          end
+        end
+      end
+
+      assign hold_last_sample = ((in_pkt_cnt == out_pkt_cnt) | (in_pkt_cnt == out_pkt_cnt+1)) & ~release_due_to_error & (pkt_size != 1);
+
+      assign i_tlast_int = i_tlast | large_pkt;
+      assign i_terror    = i_tlast & i_tvalid & (small_pkt | large_pkt);
+
+      // FIFO with ability to rewind write pointer back if input packet is flagged as bad
+      axi_drop_packet #(.WIDTH(WIDTH+1), .MAX_PKT_SIZE(MAX_PKT_SIZE)) axi_drop_packet (
+        .clk(clk), .reset(reset), .clear(clear),
+        .i_tdata({i_tlast,i_tdata}), .i_tvalid(i_tvalid), .i_tlast(i_tlast_int), .i_terror(i_terror), .i_tready(i_tready),
+        .o_tdata({int_tlast,int_tdata}), .o_tvalid(int_tvalid), .o_tlast(), .o_tready(int_tready & ~(hold_last_sample & int_tlast)));
+
+      // Generate output register to hold on to last word
+      if (HOLD_LAST_WORD) begin
+        axi_fifo_flop2 #(.WIDTH(WIDTH+1)) axi_fifo_flop2 (
+          .clk(clk), .reset(reset), .clear(clear),
+          .i_tdata({int_tlast,int_tdata}), .i_tvalid(int_tvalid & ~(hold_last_sample & int_tlast)), .i_tready(int_tready),
+          .o_tdata({o_tlast,o_tdata}), .o_tvalid(o_tvalid), .o_tready(o_tready),
+          .space(), .occupied());
+      end else begin
+        assign o_tdata    = int_tdata;
+        assign o_tlast    = int_tlast;
+        assign o_tvalid   = int_tvalid;
+        assign int_tready = o_tready;
+      end
     end
   endgenerate
 
