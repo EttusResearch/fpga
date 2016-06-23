99b536711b58a62094370da5a43b61859547cdbe
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2016-06-23 12:17:08 -0500

    rfnoc: noc_block_ddc: Set default NUM_CHAINS to 2 to match noc script

--- usrp3/lib/rfnoc/noc_block_ddc.v
+++ usrp3/lib/rfnoc/noc_block_ddc.v
@@ -5,7 +5,7 @@
 module noc_block_ddc #(
   parameter NOC_ID = 64'hDDC0_0000_0000_0000,
   parameter STR_SINK_FIFOSIZE = 11,
-  parameter NUM_CHAINS = 1
+  parameter NUM_CHAINS = 2
 )(
   input bus_clk, input bus_rst,
   input ce_clk, input ce_rst,
