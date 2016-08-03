ce30a35fa3a6003ff38d0d28f9d135d13784ed27
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2016-08-03 16:05:59 -0500

    lib: axi_fifo_flop2: Improved timing and reduced size
    - Moved data path mux to input instead of output

--- usrp3_rfnoc/lib/fifo/axi_fifo_flop2.v
+++ usrp3_rfnoc/lib/fifo/axi_fifo_flop2.v
@@ -9,76 +9,48 @@
 //       indicating i_tdata has been read. This is an AXI stream requirement.
 
 module axi_fifo_flop2 #(
-  parameter WIDTH = 32)
-(
+  parameter WIDTH = 32
+)(
   input clk,
   input reset,
   input clear,
   input [WIDTH-1:0] i_tdata,
   input i_tvalid,
   output i_tready,
-  output [WIDTH-1:0] o_tdata,
-  output o_tvalid,
+  output reg [WIDTH-1:0] o_tdata,
+  output reg o_tvalid,
   input o_tready,
   output [1:0] space,
   output [1:0] occupied);
 
-  reg [WIDTH-1:0] i_tdata_pipe  = 'd0;
-  reg [WIDTH-1:0] i_tdata_reg   = 'd0;
-  reg             i_tvalid_pipe = 1'b0;
-  reg             i_tvalid_reg  = 1'b0;
+  reg [WIDTH-1:0] i_tdata_temp;
+  reg             i_tvalid_temp;
 
-  // Steady state data flow is typically: i_tdata -> i_tdata_reg -> o_tdata
-  // but can change to: i_tdata -> i_tdata_reg -> i_tdata_pipe -> o_tdata
-  // depending on i_tvalid / o_tready combinations
+  assign i_tready = ~i_tvalid_temp;
+
   always @(posedge clk) begin
-    // Always accept new data if there is space in the pipeline
-    if (i_tready) begin
-      i_tdata_reg  <= i_tdata;
-      i_tvalid_reg <= i_tvalid;
-    end
-    if (o_tready) begin
-      if (i_tready) begin
-        // Only useful when data flow is i_tdata -> i_tdata_reg -> i_tdata_pipe -> o_tdata
-        i_tdata_pipe <= i_tdata_reg;
-        if (i_tvalid_pipe) begin
-          // Switch from: i_tdata -> i_tdata_reg -> i_tdata_pipe -> o_tdata
-          // to:          i_tdata -> i_tdata_reg -> o_tdata
-          // will occur if i_tvalid_reg = 0
-          i_tvalid_pipe <= i_tvalid_reg;
-        end
-      // Input is throttled
+    if (~o_tvalid | o_tready) begin
+      if (i_tvalid_temp) begin
+        o_tvalid      <= 1'b1;
+        o_tdata       <= i_tdata_temp;
       end else begin
-        i_tdata_pipe  <= i_tdata_reg;
-        i_tvalid_pipe <= i_tvalid_reg;
-        i_tvalid_reg  <= 1'b0; // Since i_tready = 0, we know we will not get new data this cycle
+        o_tvalid      <= i_tvalid;
+        o_tdata       <= i_tdata;
       end
-    // Output is throttled
+      i_tvalid_temp   <= 1'b0;
     end else begin
-      // Space available in i_tdata_pipe to store i_tdata_reg.
-      // This is the case where data flow will change
-      // from: i_tdata -> i_tdata_reg -> o_tdata
-      // to:   i_tdata -> i_tdata_reg -> i_tdata_pipe -> o_tdata
-      // if i_tvalid_reg = 1
-      if (~i_tvalid_pipe) begin
-        i_tvalid_pipe <= i_tvalid_reg;
-        i_tdata_pipe  <= i_tdata_reg;
+      if (~i_tvalid_temp) begin
+        i_tvalid_temp <= i_tvalid;
+        i_tdata_temp  <= i_tdata;
       end
     end
-    // This module will be used very often, so only reset
-    // tvalid paths to save on fanout.
     if (reset | clear) begin
-      i_tvalid_reg  <= 1'b0;
-      i_tvalid_pipe <= 1'b0;
+      o_tvalid      <= 1'b0;
+      i_tvalid_temp <= 1'b0;
     end
   end
 
-  assign i_tready = ~reset & ~(i_tvalid_reg & i_tvalid_pipe);
-
-  assign o_tvalid = i_tvalid_pipe ? 1'b1         : i_tvalid_reg;
-  assign o_tdata  = i_tvalid_pipe ? i_tdata_pipe : i_tdata_reg;
-
-  assign occupied = i_tvalid_reg + i_tvalid_pipe;
-  assign space = 2'd2 - occupied;
+  assign occupied = i_tvalid_temp + o_tvalid;
+  assign space    = 2 - occupied;
 
 endmodule
