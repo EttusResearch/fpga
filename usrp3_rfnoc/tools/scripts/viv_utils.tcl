b264281c4d995a41cfbc59fdaa4d5a2a492b2846
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2016-08-09 12:52:06 -0500

    tools: Added area optimization to synthesis

--- usrp3_rfnoc/tools/scripts/viv_utils.tcl
+++ usrp3_rfnoc/tools/scripts/viv_utils.tcl
@@ -108,6 +108,7 @@ proc ::vivado_utils::synthesize_design {args} {
     set synth_cmd [concat $synth_cmd $vdef_args]
     set synth_cmd [concat $synth_cmd $incdir_args]
     set synth_cmd [concat $synth_cmd $args]
+    set synth_cmd [concat $synth_cmd "-directive AreaOptimized_high -control_set_opt_threshold 1"]
     puts "BUILDER: Synthesizing design"
     eval $synth_cmd
 }
