d5eee58655d4c775df5215969bc0c6b41425ca67
michael-west <michael.west@ettus.com>
2016-08-31 15:22:52 -0700

    X300: Increase buffer for GPS UART (needed for reduced firmware clock rate)

--- usrp3_rfnoc/lib/wishbone/simple_uart_rx.v
+++ usrp3_rfnoc/lib/wishbone/simple_uart_rx.v
@@ -6,9 +6,9 @@
 
 
 module simple_uart_rx
-    #(parameter SIZE=0)
-    (input clk, input rst, 
-     output [7:0] fifo_out, input fifo_read, output [5:0] fifo_level, output fifo_empty, 
+    #(parameter SIZE=8)
+    (input clk, input rst,
+     output [7:0] fifo_out, input fifo_read, output [15:0] fifo_level, output fifo_empty,
      input [15:0] clkdiv, input rx);
 
    reg 		  rx_d1, rx_d2;
@@ -17,7 +17,7 @@ module simple_uart_rx
        {rx_d2,rx_d1} <= 0;
      else
        {rx_d2,rx_d1} <= {rx_d1,rx};
-   
+
    reg [15:0] 	  baud_ctr;
    reg [3:0] 	  bit_ctr;
    reg [7:0] 	  sr;
@@ -26,13 +26,13 @@ module simple_uart_rx
    wire 	  shift_now = baud_ctr == (clkdiv>>1);
    wire 	  stop_now = (bit_ctr == 10) && shift_now;
    wire 	  go_now = (bit_ctr == 0) && neg_trans;
-   
+
    always @(posedge clk)
      if(rst)
        sr <= 0;
      else if(shift_now)
        sr <= {rx_d2,sr[7:1]};
-   
+
    always @(posedge clk)
      if(rst)
        baud_ctr <= 0;
@@ -49,7 +49,7 @@ module simple_uart_rx
    always @(posedge clk)
      if(rst)
        bit_ctr <= 0;
-     else 
+     else
        if(go_now)
 	 bit_ctr <= 1;
        else if(stop_now)
@@ -61,11 +61,11 @@ module simple_uart_rx
    wire 	  full = ~i_tready;
    wire 	  write = ~full & rx_d2 & stop_now;
    assign fifo_empty = ~o_tvalid;
-   
+
    axi_fifo #(.WIDTH(8), .SIZE(SIZE)) fifo
      (.clk(clk),.reset(rst), .clear(1'b0),
       .i_tdata(sr),.i_tvalid(write),.i_tready(i_tready),
       .o_tdata(fifo_out),.o_tvalid(o_tvalid),.o_tready(fifo_read),
       .space(),.occupied(fifo_level) );
-   
+
 endmodule // simple_uart_rx
