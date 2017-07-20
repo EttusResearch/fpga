44d5d9adc8142ab0df513505bd1927a5a8f2bb1f
Alex Williams <alex.williams@ni.com>
2017-07-20 15:08:24 -0700

    lib: Added converter between axi-stream and cvita.
    Mostly, this is for IP Integrator's tracking of interfaces, but CHDR does not
    follow convention with its bus layout. (Convention is to put first 32-bit word
    in lower indices, not upper as done in CHDR IPs.) Words are swapped vs. AXI-S.
    
    Also, the N310 implementation was updated to use the bus properly.
    
    Signed-of-by: Alex Williams <alex.williams@ni.com>
    Signed-of-by: Moritz Fischer <moritz.fischer@ettus.com>

---
+++ usrp3/lib/packet_proc/axis_to_cvita.v
@@ -0,0 +1,27 @@
+`default_nettype none
+
+module axis_to_cvita
+(
+        input wire           clk,
+
+	input wire [63:0]    s_axis_tdata,
+        input wire           s_axis_tlast,
+        input wire           s_axis_tvalid,
+        output wire          s_axis_tready,
+
+	output wire [63:0]   o_tdata,
+        output wire          o_tlast,
+        output wire          o_tvalid,
+        input wire           o_tready
+);
+
+	assign s_axis_tready = o_tready;
+
+	assign o_tdata = {s_axis_tdata[31:0], s_axis_tdata[63:32]};
+	assign o_tlast = s_axis_tlast;
+	assign o_tvalid = s_axis_tvalid;
+
+endmodule // axis_to_cvita
+
+`default_nettype wire
+
