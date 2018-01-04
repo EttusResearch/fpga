3226c919cbf94f2b3ad224400cd8fd69bd065686
Trung N Tran <trung.tran@ettus.com>
2018-01-03 16:04:01 -0800

    lib: settings_bus_mux.v by pass axi_mux
    When there's only 1 channel, we don't need the mux.

--- usrp3/lib/control/settings_bus_mux.v
+++ usrp3/lib/control/settings_bus_mux.v
@@ -16,23 +16,26 @@ module settings_bus_mux #(
 );
 
   wire [NUM_BUSES*(AWIDTH+DWIDTH)-1:0] i_tdata;
-
-  genvar i;
   generate
-    for (i = 0; i < NUM_BUSES; i = i + 1) begin
-      assign i_tdata[(i+1)*(AWIDTH+DWIDTH)-1:i*(AWIDTH+DWIDTH)] = {in_set_addr[(i+1)*AWIDTH-1:i*AWIDTH],in_set_data[(i+1)*DWIDTH-1:i*DWIDTH]};
+    if(NUM_BUSES <= 1) begin
+      assign out_set_stb = in_set_stb;
+      assign out_set_addr = in_set_addr;
+      assign out_set_data = in_set_data;
+    end else begin
+      genvar i;
+        for (i = 0; i < NUM_BUSES; i = i + 1) begin
+          assign i_tdata[(i+1)*(AWIDTH+DWIDTH)-1:i*(AWIDTH+DWIDTH)] = {in_set_addr[(i+1)*AWIDTH-1:i*AWIDTH],in_set_data[(i+1)*DWIDTH-1:i*DWIDTH]};
+        end
+      axi_mux #(
+        .PRIO(PRIO),
+        .WIDTH(AWIDTH+DWIDTH),
+        .PRE_FIFO_SIZE($clog2(NUM_BUSES)),
+        .POST_FIFO_SIZE(FIFO_SIZE),
+        .SIZE(NUM_BUSES))
+      axi_mux (
+        .clk(clk), .reset(reset), .clear(clear),
+        .i_tdata(i_tdata), .i_tlast({NUM_BUSES{1'b1}}), .i_tvalid(in_set_stb), .i_tready(),
+        .o_tdata({out_set_addr,out_set_data}), .o_tlast(), .o_tvalid(out_set_stb), .o_tready(ready));
     end
   endgenerate
-
-  axi_mux #(
-    .PRIO(PRIO),
-    .WIDTH(AWIDTH+DWIDTH),
-    .PRE_FIFO_SIZE($clog2(NUM_BUSES)),
-    .POST_FIFO_SIZE(FIFO_SIZE),
-    .SIZE(NUM_BUSES))
-  axi_mux (
-    .clk(clk), .reset(reset), .clear(clear),
-    .i_tdata(i_tdata), .i_tlast({NUM_BUSES{1'b1}}), .i_tvalid(in_set_stb), .i_tready(),
-    .o_tdata({out_set_addr,out_set_data}), .o_tlast(), .o_tvalid(out_set_stb), .o_tready(ready));
-
 endmodule
