d6a878b6b02d4692940da1a21a1b38993f0705b5
Trung Tran <trung.tran@ettus.com>
2018-05-21 23:55:50 -0700

    rfnoc_ce_default_inst_e310: change noc_block_duc id
    We introduce new noc_block_duc id based on number of input ports.

--- usrp3/top/e300/rfnoc_ce_default_inst_e310.v
+++ usrp3/top/e300/rfnoc_ce_default_inst_e310.v
@@ -24,7 +24,7 @@
     .o_tdata(ce_i_tdata[0]), .o_tlast(ce_i_tlast[0]), .o_tvalid(ce_i_tvalid[0]), .o_tready(ce_i_tready[0]),
     .debug(ce_debug[0]));
 
-  noc_block_duc #( .NUM_CHAINS(2), .NUM_HB(1), .CIC_MAX_INTERP(8)) inst_noc_block_duc (
+  noc_block_duc #( .NUM_CHAINS(2), .NUM_HB(1), .CIC_MAX_INTERP(8), .NOC_ID(64'hD0C0_0000_0000_0002)) inst_noc_block_duc (
     .bus_clk(bus_clk), .bus_rst(bus_rst),
     .ce_clk(ce_clk), .ce_rst(ce_rst),
     .i_tdata(ce_o_tdata[1]), .i_tlast(ce_o_tlast[1]), .i_tvalid(ce_o_tvalid[1]), .i_tready(ce_o_tready[1]),
