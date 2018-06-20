50e352eab1f4d071373c7f35d950d84147e11588
Ashish Chaudhari <ashish@ettus.com>
2018-06-20 12:43:16 -0700

    tools: Added reporting capability to run_testbenches
    - The script can now search the filesystem for log files
      for TBs and generate an overall health report

--- usrp3/tools/utils/run_testbenches.py
+++ usrp3/tools/utils/run_testbenches.py
@@ -34,21 +34,22 @@ _STDOUT.setFormatter(_FORMATTER)
 RETCODE_SUCCESS     = 0
 RETCODE_PARSE_ERR   = -1
 RETCODE_EXEC_ERR    = -2
-RETCODE_UNKNOWN_ERR = -3
+RETCODE_COMPILE_ERR = -3
+RETCODE_UNKNOWN_ERR = -4
 
 def retcode_to_str(code):
     """ Convert internal status code to string
     """
-    if code == RETCODE_SUCCESS:
-        return 'Success'
-    elif code > RETCODE_SUCCESS:
-        return 'AppError({code}})'.format(code=code)
+    code = int(code)
+    if code > RETCODE_SUCCESS:
+        return 'AppError({code})'.format(code=code)
     else:
-        msgs = {
+        return {RETCODE_SUCCESS:'OK',
             RETCODE_PARSE_ERR:'ParseError', 
             RETCODE_EXEC_ERR:'ExecError', 
-            RETCODE_UNKNOWN_ERR:'UnknownError'}
-        return msgs[code]
+            RETCODE_COMPILE_ERR:'CompileError', 
+            RETCODE_UNKNOWN_ERR:'UnknownError'
+        }[code]
 
 def log_with_header(what, minlen = 0, ch = '#'):
     """ Print with a header around the text
@@ -63,20 +64,90 @@ def log_with_header(what, minlen = 0, ch = '#'):
 # Simulation Functions
 #-------------------------------------------------------
 
-def find_sims(base_dir):
-    """ Find all testbenches in the specific base_dir
+def find_sims_on_fs(basedir):
+    """ Find all testbenches in the specific basedir
         Testbenches are defined as directories with a
         Makefile that includes viv_sim_preamble.mak
     """
     sims = {}
-    for root, _, files in os.walk(base_dir):
+    for root, _, files in os.walk(basedir):
         if 'Makefile' in files:
             with open(os.path.join(root, 'Makefile'), 'r') as mfile:
                 for l in mfile.readlines():
                     if re.match('.*include.*viv_sim_preamble.mak.*', l) is not None:
-                        sims.update({os.path.relpath(root, base_dir): root})
+                        sims.update({os.path.relpath(root, basedir): root})
                         break
     return sims
+
+def gather_target_sims(basedir, targets):
+    """ Parse the specified targets and gather simulations to run
+        Remove duplicates and sort alphabetically
+    """
+    fs_sims = find_sims_on_fs(basedir)
+    if not isinstance(targets, list):
+        targets = [targets]
+    sim_names = set()
+    for target in targets:
+        for name in sorted(fs_sims):
+            if re.match(target, name) is not None:
+                sim_names.add(name)
+    target_sims = []
+    for name in sorted(sim_names):
+        target_sims.append((name, fs_sims[name]))
+    return target_sims
+
+def parse_output(simout):
+    # Gather results (basic metrics)
+    results = {'retcode':RETCODE_SUCCESS, 'stdout':simout, 'passed':False}
+    # Look for the following in the log:
+    # - A start timestamp (indicates that Vivado started)
+    # - The testbench infrastructure start header (indicates that the TB started)
+    # - A stop timestamp (indicates that the TB stopped)
+    tb_started = False
+    compile_started = False
+    results['start_time'] = '<unknown>'
+    results['wall_time'] = '<unknown>'
+    for line in simout.split(b'\n'):
+        tsm = re.match(rb'TESTBENCH STARTED: (.+)', line)
+        if tsm is not None:
+            tb_started = True
+        csm = re.match(rb'source .*viv_sim_project.tcl', line)
+        if csm is not None:
+            compile_started = True
+        vsm = re.match(rb'# Start of session at: (.+)', line)
+        if vsm is not None:
+            results['start_time'] = str(vsm.group(1), 'ascii')
+        tfm = re.match(rb'launch_simulation:.*; elapsed = (.+) \..*', line)
+        if tfm is not None:
+            results['wall_time'] = str(tfm.group(1), 'ascii')
+    # Parse testbench results
+    tb_match_arr = ([
+        b'.*TESTBENCH FINISHED: (.+)\n',
+        b' - Time elapsed:   (.+) ns.*\n',
+        b' - Tests Expected: (.+)\n',
+        b' - Tests Run:      (.+)\n',
+        b' - Tests Passed:   (.+)\n',
+        b'Result: (PASSED|FAILED).*',
+    ])
+    m = re.match(b''.join(tb_match_arr), simout, re.DOTALL)
+    # Figure out the returncode 
+    retcode = RETCODE_UNKNOWN_ERR
+    if m is not None:
+        retcode = RETCODE_SUCCESS
+        results['passed'] = (m.group(6) == b'PASSED')
+        results['module'] = m.group(1)
+        results['sim_time_ns'] = int(m.group(2))
+        results['tc_expected'] = int(m.group(3))
+        results['tc_run'] = int(m.group(4))
+        results['tc_passed'] = int(m.group(5))
+    elif tb_started:
+        retcode = RETCODE_PARSE_ERR
+    elif compile_started:
+        retcode = RETCODE_COMPILE_ERR
+    else:
+        retcode = RETCODE_EXEC_ERR
+    results['retcode'] = retcode
+    return results
 
 def run_sim(path, simulator, basedir, setupenv):
     """ Run the simulation at the specified path
@@ -85,6 +156,7 @@ def run_sim(path, simulator, basedir, setupenv):
     """
     try:
         os.chdir(os.path.join(basedir, path))
+        # Optionally run an environment setup script
         if setupenv is None:
             setupenv = ''
             # Check if environment was setup
@@ -92,26 +164,12 @@ def run_sim(path, simulator, basedir, setupenv):
                 raise RuntimeError('Simulation environment was uninitialized') 
         else:
             setupenv = '. ' + os.path.realpath(setupenv) + ';'
-        simout = subprocess.check_output(
-            '{setupenv} make {simulator} 2>&1'.format(setupenv=setupenv, simulator=simulator), shell=True)
-        tb_match_arr = ([
-            b'.*TESTBENCH FINISHED: (.+)\n',
-            b' - Time elapsed:   (.+) ns.*\n',
-            b' - Tests Expected: (.+)\n',
-            b' - Tests Run:      (.+)\n',
-            b' - Tests Passed:   (.+)\n',
-            b'Result: (PASSED|FAILED).*',
-        ])
-        m = re.match(b''.join(tb_match_arr), simout, re.DOTALL)
-        if m is not None:
-            return {'retcode': 0, 'passed':(m.group(6) == b'PASSED'), 
-                    'module':m.group(1), 'ns_elapsed':int(m.group(2)), 
-                    'tc_expected':int(m.group(3)), 'tc_run':int(m.group(4)), 'tc_passed':int(m.group(5)),
-                    'stdout':simout}
-        else:
-            return {'retcode': RETCODE_PARSE_ERR, 'passed':False, 'stdout':simout}
+        # Run the simulation
+        return parse_output(
+            subprocess.check_output(
+                '{setupenv} make {simulator} 2>&1'.format(setupenv=setupenv, simulator=simulator), shell=True))
     except subprocess.CalledProcessError as e:
-        return {'retcode': abs(e.returncode), 'passed':False, 'stdout':e.output}
+        return {'retcode': int(abs(e.returncode)), 'passed':False, 'stdout':e.output}
     except Exception as e:
         _LOG.error('Target ' + path + ' failed to run:\n' + str(e))
         return {'retcode': RETCODE_EXEC_ERR, 'passed':False, 'stdout':bytes(str(e), 'utf-8')}
@@ -147,14 +205,8 @@ def run_sim_queue(run_queue, out_queue, simulator, basedir, setupenv):
 def do_list(args):
     """ List all simulations that can be run
     """
-    sims = find_sims(args.basedir)
-    result_all = 0
-    if not isinstance(args.target, list):
-        args.target = [args.target]
-    for target in args.target:
-        for name in sorted(sims):
-            if re.match(target, name) is not None:
-                print(name)
+    for (name, path) in gather_target_sims(args.basedir, args.target):
+        print(name)
     return 0
 
 def do_run(args):
@@ -164,17 +216,12 @@ def do_run(args):
     run_queue = Queue(maxsize=0)
     out_queue = Queue(maxsize=0)
     _LOG.info('Queueing the following targets to simulate:')
-    sims = find_sims(args.basedir)
-    if not isinstance(args.target, list):
-        args.target = [args.target]
-    for target in args.target:
-        for name in sorted(sims):
-            if re.match(target, name) is not None:
-                run_queue.put((name, sims[name]))
-                _LOG.info('* ' + name)
+    for (name, path) in gather_target_sims(args.basedir, args.target):
+        run_queue.put((name, path))
+        _LOG.info('* ' + name)
     # Spawn tasks to run builds
     num_sims = run_queue.qsize()
-    num_jobs = min(num_sims, int(args.threads))
+    num_jobs = min(num_sims, int(args.jobs))
     _LOG.info('Starting ' + str(num_jobs) + ' job(s) to process queue...')
     results = {}
     for i in range(num_jobs):
@@ -203,14 +250,15 @@ def do_run(args):
         if not result['passed']:
             result_all += 1
 
-    log_with_header('RESULTS', 30)
+    log_with_header('RESULTS', 40)
     for name in results:
         r = results[name]
         if 'module' in r:
-            _LOG.info('* %s : %s (Expected=%d, Run=%d, Passed=%d)',
-                ('PASS' if r['passed'] else 'FAIL'), name, r['tc_expected'], r['tc_run'], r['tc_passed'])
+            _LOG.info('* %s : %s (Expected=%d, Run=%d, Passed=%d, Elapsed=%s)',
+                ('PASS' if r['passed'] else 'FAIL'), name, r['tc_expected'], r['tc_run'], r['tc_passed'], r['wall_time'])
         else:
-            _LOG.info('* %s : %s (Status=%s)', ('PASS' if r['passed'] else 'FAIL'), name, retcode_to_str(r['retcode']))
+            _LOG.info('* %s : %s (Status=%s)', ('PASS' if r['passed'] else 'FAIL'), 
+                name, retcode_to_str(r['retcode']))
     return result_all
 
 
@@ -225,15 +273,38 @@ def do_cleanup(args):
             raise RuntimeError('Simulation environment was uninitialized') 
     else:
         setupenv = '. ' + os.path.realpath(setupenv) + ';'
-    sims = find_sims(args.basedir)
-    if not isinstance(args.target, list):
-        args.target = [args.target]
-    for target in args.target:
-        for name in sorted(sims):
-            if re.match(target, name) is not None:
-                _LOG.info('Cleaning up %s', name)
-                os.chdir(os.path.join(args.basedir, sims[name]))
-                subprocess.Popen('{setupenv} make cleanall'.format(setupenv=setupenv), shell=True).wait()
+    for (name, path) in gather_target_sims(args.basedir, args.target):
+        _LOG.info('Cleaning up %s', name)
+        os.chdir(os.path.join(args.basedir, path))
+        subprocess.Popen('{setupenv} make cleanall'.format(setupenv=setupenv), shell=True).wait()
+    return 0
+
+def do_report(args):
+    """ List all simulations that can be run
+    """
+    keys = ['module', 'status', 'retcode', 'start_time', 'wall_time',
+            'sim_time_ns', 'tc_expected', 'tc_run', 'tc_passed']
+    with open(args.repfile, 'w') as repfile:
+        repfile.write((','.join([x.upper() for x in keys])) + '\n')
+        for (name, path) in gather_target_sims(args.basedir, args.target):
+            results = {'module': str(name), 'status':'NOT_RUN', 'retcode':'<unknown>', 
+                       'start_time':'<unknown>', 'wall_time':'<unknown>', 'sim_time_ns':0,
+                       'tc_expected':0, 'tc_run':0, 'tc_passed':0}
+            logpath = os.path.join(path, args.simulator + '.log')
+            if os.path.isfile(logpath):
+                with open(logpath, 'rb') as logfile:
+                    r = parse_output(logfile.read())
+                    if r['retcode'] != RETCODE_SUCCESS:
+                        results['retcode'] = retcode_to_str(r['retcode'])
+                        results['status'] = 'ERROR'
+                        results['start_time'] = r['start_time']
+                    else:
+                        results = r
+                        results['module'] = name
+                        results['status'] = 'PASSED' if r['passed'] else 'FAILED'
+                        results['retcode'] = retcode_to_str(r['retcode'])
+            repfile.write((','.join([str(results[x]) for x in keys])) + '\n')
+    _LOG.info('Testbench report written to ' + args.repfile)
     return 0
 
 # Parse command line options
@@ -242,14 +313,15 @@ def get_options():
     parser.add_argument('--basedir', default=BASE_DIR, help='Base directory for the usrp3 codebase')
     parser.add_argument('--simulator', choices=['xsim', 'vsim'], default='xsim', help='Simulator name')
     parser.add_argument('--setupenv', default=None, help='Optional environment setup script to run for each TB')
-    parser.add_argument('--threads', default=4, help='Number of parallel simulations to run')
-    parser.add_argument('action', choices=['run', 'cleanup', 'list'], default='list', help='What to do?')
+    parser.add_argument('-j', '--jobs', default=1, help='Number of parallel simulation jobs to run')
+    parser.add_argument('--repfile', default='testbench_report.csv', help='Name of the output report file')
+    parser.add_argument('action', choices=['run', 'cleanup', 'list', 'report'], default='list', help='What to do?')
     parser.add_argument('target', nargs='*', default='.*', help='Space separated simulation target regexes')
     return parser.parse_args()
 
 def main():
     args = get_options()
-    actions = {'list': do_list, 'run': do_run, 'cleanup': do_cleanup}
+    actions = {'list': do_list, 'run': do_run, 'cleanup': do_cleanup, 'report': do_report}
     return actions[args.action](args)
 
 if __name__ == '__main__':
