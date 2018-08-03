fa73bc738b8b6780edf29be47f2b56f3bd655e8d
Ashish Chaudhari <ashish@ettus.com>
2018-08-03 14:06:33 -0700

    e300: More aggressive place/route options
    - Removed -control_set_opt_threshold from synthesis. The tool
      defaults actually give better QoS
    - Switched placer to use pessimistic net delays to make the routers
      job easier. BBFC added more congestion to the design which was
      making routing hard
    - Made the various optimization steps more aggressive

--- usrp3/top/e300/build_e300.tcl
+++ usrp3/top/e300/build_e300.tcl
@@ -9,11 +9,23 @@ source $::env(VIV_TOOLS_DIR)/scripts/viv_strategies.tcl
 vivado_utils::initialize_project
 
 # STEP#2: Run synthesis 
-vivado_utils::synthesize_design -directive AreaOptimized_high -control_set_opt_threshold 1
+vivado_utils::synthesize_design -directive AreaOptimized_high
 vivado_utils::generate_post_synth_reports
 
 # STEP#3: Run implementation strategy
-vivado_strategies::implement_design [vivado_strategies::get_impl_preset "Performance_ExplorePostRoutePhysOpt"]
+set e3xx_strategy [dict create]
+dict set e3xx_strategy "opt_design.is_enabled"                   1
+dict set e3xx_strategy "opt_design.directive"                    "NoBramPowerOpt"
+dict set e3xx_strategy "post_opt_power_opt_design.is_enabled"    0
+dict set e3xx_strategy "place_design.directive"                  "ExtraNetDelay_high"
+dict set e3xx_strategy "post_place_power_opt_design.is_enabled"  0
+dict set e3xx_strategy "post_place_phys_opt_design.is_enabled"   1
+dict set e3xx_strategy "post_place_phys_opt_design.directive"    "AggressiveExplore"
+dict set e3xx_strategy "route_design.directive"                  "Explore"
+dict set e3xx_strategy "route_design.more_options"               "-tns_cleanup"
+dict set e3xx_strategy "post_route_phys_opt_design.is_enabled"   1
+dict set e3xx_strategy "post_route_phys_opt_design.directive"    "Explore"
+vivado_strategies::implement_design $e3xx_strategy
 
 # STEP#4: Generate reports
 vivado_utils::generate_post_route_reports
