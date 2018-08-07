7f020289c644f2b320554eb2f1d9369e6dc35395
Martin Braun <martin.braun@ettus.com>
2018-08-07 13:30:36 -0500

    lib: Improve docs for axi_round_and_clip

--- usrp3/lib/rfnoc/axi_round_and_clip.v
+++ usrp3/lib/rfnoc/axi_round_and_clip.v
@@ -1,10 +1,24 @@
-
-
+//
 // Copyright 2014, Ettus Research
 // Copyright 2018 Ettus Research, a National Instruments Company
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
-
+//
+//
+// Reduce the bitwidth of an input stream. This module will both round
+// and clip, meaning that the lower bits will be removed by rounding to
+// the nearest value, and the top bits will be snipped (but without
+// causing numerical overflows).
+//
+// Example: If WIDTH_IN==24, WIDTH_OUT==16, and CLIP_BITS==3, the output
+// Will remove the top 3 bits (by clipping), and remove the bottom 5
+// bits by rounding, leaving 24 - 3 - 5 == 16 bits.
+//
+// Note that this module has two stages (round, then clip) which will
+// both have a FIFO of length FIFOSIZE. However, when a stage is not
+// required it will have *no* FIFO, but instead just pass through the
+// data. In the extreme case where WIDTH_IN==WIDTH_OUT and CLIP_BITS==0,
+// there are no FIFOs and this module becomes an AXI stream passthrough.
 module axi_round_and_clip
 #(
   parameter WIDTH_IN=24,
