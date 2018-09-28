7139a4eb7acf1a72f82fce5e57790ca1623cfec8
Ciro Nishiguchi <ciro.nishiguchi@ni.com>
2018-09-28 15:50:51 -0500

    rfnoc: Instantiate traffic counter in null source

--- usrp3/lib/rfnoc/noc_block_null_source_sink.v
+++ usrp3/lib/rfnoc/noc_block_null_source_sink.v
@@ -7,7 +7,8 @@
 
 module noc_block_null_source_sink #(
   parameter NOC_ID = 64'h0000_0000_0000_0000,
-  parameter STR_SINK_FIFOSIZE = 11)
+  parameter STR_SINK_FIFOSIZE = 11,
+  parameter EN_TRAFFIC_COUNTER = 0)
 (
   input bus_clk, input bus_rst,
   input ce_clk, input ce_rst,
@@ -24,6 +25,10 @@ module noc_block_null_source_sink #(
   wire [31:0] set_data;
   wire [7:0]  set_addr;
   wire        set_stb;
+
+  reg  [63:0] rb_data;
+  wire [7:0]  rb_addr;
+  reg         rb_stb;
 
   wire [63:0] cmdout_tdata, ackin_tdata;
   wire        cmdout_tlast, cmdout_tvalid, cmdout_tready, ackin_tlast, ackin_tvalid, ackin_tready;
@@ -47,7 +52,7 @@ module noc_block_null_source_sink #(
     // Control Sink
     .vita_time(),
     .set_data(set_data), .set_addr(set_addr), .set_stb(set_stb), .set_has_time(), .set_time(),
-    .rb_stb(1'b1), .rb_data(64'd0), .rb_addr(),
+    .rb_stb(rb_stb), .rb_data(rb_data), .rb_addr(rb_addr),
     // Control Source
     .cmdout_tdata(cmdout_tdata), .cmdout_tlast(cmdout_tlast), .cmdout_tvalid(cmdout_tvalid), .cmdout_tready(cmdout_tready),
     .ackin_tdata(ackin_tdata), .ackin_tlast(ackin_tlast), .ackin_tvalid(ackin_tvalid), .ackin_tready(ackin_tready),
@@ -86,6 +91,10 @@ module noc_block_null_source_sink #(
   localparam SR_LINE_RATE           = 130;
   localparam SR_ENABLE_STREAM       = 131;
 
+  // Leave some space for future null source registers
+  localparam TRAFFIC_COUNTER_SR_BASE  = 192;
+  localparam TRAFFIC_COUNTER_RB_BASE  = 64;
+
   null_source #(
     .SR_LINES_PER_PACKET(SR_LINES_PER_PACKET),
     .SR_LINE_RATE(SR_LINE_RATE),
@@ -95,5 +104,45 @@ module noc_block_null_source_sink #(
     .sid({src_sid, next_dst_sid}),
     .set_stb(set_stb), .set_addr(set_addr), .set_data(set_data),
     .o_tdata(str_src_tdata), .o_tlast(str_src_tlast), .o_tvalid(str_src_tvalid), .o_tready(str_src_tready));
+
+  generate
+
+    if (EN_TRAFFIC_COUNTER) begin : tc
+
+      wire [63:0] tc_rb_data;
+      wire        tc_rb_stb;
+
+      noc_traffic_counter #(
+        .SR_REG_BASE(TRAFFIC_COUNTER_SR_BASE), .RB_REG_BASE(TRAFFIC_COUNTER_RB_BASE)
+      ) traffic_counter(
+        .bus_clk(bus_clk), .bus_rst(bus_rst),
+        .ce_clk(ce_clk), .ce_rst(ce_rst),
+        .set_data(set_data), .set_addr(set_addr), .set_stb(set_stb),
+        .rb_stb(tc_rb_stb), .rb_addr(rb_addr), .rb_data(tc_rb_data),
+        .i_tlast(i_tlast), .i_tvalid(i_tvalid), .i_tready(i_tready),
+        .o_tlast(o_tlast), .o_tvalid(o_tvalid), .o_tready(o_tready),
+        .str_sink_tlast(str_sink_tlast), .str_sink_tvalid(str_sink_tvalid), .str_sink_tready(str_sink_tready),
+        .str_src_tlast(str_src_tlast_bclk), .str_src_tvalid(str_src_tvalid_bclk), .str_src_tready(str_src_tready_bclk));
+
+      // Mux read-back registers
+      always @(posedge ce_clk) begin
+        if (rb_addr < TRAFFIC_COUNTER_RB_BASE) begin
+          rb_stb  <= 1'd1;
+          rb_data <= 64'h0BADC0DE0BADC0DE;
+        end else begin
+          rb_stb  <= tc_rb_stb;
+          rb_data <= tc_rb_data;
+        end
+      end
+
+    end else begin // !EN_TRAFFIC_COUNTER
+
+      always @(posedge ce_clk) begin
+        rb_stb  <= 1'd1;
+        rb_data <= 64'h0BADC0DE0BADC0DE;
+      end
+
+    end
+  endgenerate
 
 endmodule
