4acfa70addf261e63ff284d2939a03c1a092302f
Ashish Chaudhari <ashish@ettus.com>
2019-03-06 18:12:56 -0800

    rfnoc: Add rfnoc_block_null_src_sink
    - Add a hand-written generic noc_shell module
    - Add Null source/sink block with two input/output ports
      * One port pair is a null src/sink the other is a loopback
    - Add testbench for new block

---
+++ usrp3/lib/erfnoc/blocks/rfnoc_block_null_src_sink/rfnoc_block_null_src_sink_tb.sv
@@ -0,0 +1,276 @@
+//
+// Copyright 2019 Ettus Research, A National Instruments Company
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+// Module: rfnoc_block_null_src_sink_tb
+//
+
+`default_nettype none
+
+import PkgTestExec::*;
+import PkgChdrUtils::*;
+import PkgRfnocBlockCtrlBfm::*;
+
+module rfnoc_block_null_src_sink_tb;
+
+  // Simulation Timing
+  timeunit 1ns;
+  timeprecision 1ps;
+
+  // Parameters
+  localparam [9:0]  THIS_PORTID = 10'h17;
+  localparam [15:0] THIS_EPID   = 16'hDEAD;
+  localparam int    CHDR_W      = 64;
+  localparam int    LPP         = 100;
+  localparam int    NUM_PKTS    = 50;
+
+  localparam int    PORT_SRCSNK = 0;
+  localparam int    PORT_LOOP   = 1;
+
+  // Clock and Reset Definition
+  bit rfnoc_chdr_clk, rfnoc_chdr_rst;
+  sim_clock_gen #(2.5) rfnoc_chdr_clk_gen (rfnoc_chdr_clk, rfnoc_chdr_rst); // 400 MHz
+
+  // ----------------------------------------
+  // Instantiate DUT
+  // ----------------------------------------
+
+  // Connections to DUT as interfaces:
+  RfnocBackendIf        backend (rfnoc_chdr_clk, rfnoc_chdr_clk);   // Required backend iface
+  AxiStreamIf #(32)     m_ctrl  (rfnoc_chdr_clk, rfnoc_chdr_rst);   // Required control iface
+  AxiStreamIf #(32)     s_ctrl  (rfnoc_chdr_clk, rfnoc_chdr_rst);   // Required control iface
+  AxiStreamIf #(CHDR_W) m0_chdr (rfnoc_chdr_clk, rfnoc_chdr_rst);   // Optional data iface
+  AxiStreamIf #(CHDR_W) m1_chdr (rfnoc_chdr_clk, rfnoc_chdr_rst);   // Optional data iface
+  AxiStreamIf #(CHDR_W) s0_chdr (rfnoc_chdr_clk, rfnoc_chdr_rst);   // Optional data iface
+  AxiStreamIf #(CHDR_W) s1_chdr (rfnoc_chdr_clk, rfnoc_chdr_rst);   // Optional data iface
+
+  // Bus functional model for a software block controller
+  RfnocBlockCtrlBfm #(.CHDR_W(CHDR_W), .NUM_PORTS(2)) blk_ctrl;
+
+  // DUT
+  rfnoc_block_null_src_sink #(
+    .THIS_PORTID        (THIS_PORTID),
+    .CHDR_W             (CHDR_W),
+    .NSPC               (2),
+    .MTU                (10)
+  ) dut (
+    .rfnoc_chdr_clk     (backend.chdr_clk),
+    .rfnoc_chdr_rst     (backend.chdr_rst),
+    .rfnoc_ctrl_clk     (backend.ctrl_clk),
+    .rfnoc_ctrl_rst     (backend.ctrl_rst),
+    .rfnoc_core_config  (backend.slave.cfg),
+    .rfnoc_core_status  (backend.slave.sts),
+    .s_rfnoc_chdr_tdata ({m1_chdr.slave.tdata  , m0_chdr.slave.tdata  }),
+    .s_rfnoc_chdr_tlast ({m1_chdr.slave.tlast  , m0_chdr.slave.tlast  }),
+    .s_rfnoc_chdr_tvalid({m1_chdr.slave.tvalid , m0_chdr.slave.tvalid }),
+    .s_rfnoc_chdr_tready({m1_chdr.slave.tready , m0_chdr.slave.tready }),
+    .m_rfnoc_chdr_tdata ({s1_chdr.master.tdata , s0_chdr.master.tdata }),
+    .m_rfnoc_chdr_tlast ({s1_chdr.master.tlast , s0_chdr.master.tlast }),
+    .m_rfnoc_chdr_tvalid({s1_chdr.master.tvalid, s0_chdr.master.tvalid}),
+    .m_rfnoc_chdr_tready({s1_chdr.master.tready, s0_chdr.master.tready}),
+    .s_rfnoc_ctrl_tdata (m_ctrl.slave.tdata  ),
+    .s_rfnoc_ctrl_tlast (m_ctrl.slave.tlast  ),
+    .s_rfnoc_ctrl_tvalid(m_ctrl.slave.tvalid ),
+    .s_rfnoc_ctrl_tready(m_ctrl.slave.tready ),
+    .m_rfnoc_ctrl_tdata (s_ctrl.master.tdata ),
+    .m_rfnoc_ctrl_tlast (s_ctrl.master.tlast ),
+    .m_rfnoc_ctrl_tvalid(s_ctrl.master.tvalid),
+    .m_rfnoc_ctrl_tready(s_ctrl.master.tready)
+  );
+
+  // ----------------------------------------
+  // Test Process
+  // ----------------------------------------
+  TestExec test;
+  initial begin
+    // Shared Variables
+    // ----------------------------------------
+    timeout_t    timeout;
+    ctrl_word_t  rvalue = 0;
+
+    // Initialize
+    // ----------------------------------------
+    test = new("rfnoc_block_null_src_sink_tb");
+    test.start_tb();
+
+    // Start the stream endpoint BFM
+    blk_ctrl = new(backend, m_ctrl, s_ctrl,
+      '{m0_chdr, m1_chdr}, '{s0_chdr, s1_chdr});
+    blk_ctrl.run();
+
+    // Reset
+    // ----------------------------------------
+    rfnoc_chdr_clk_gen.reset();
+
+    test.start_test("Wait for reset");
+    test.start_timeout(timeout, 1us, "Waiting for reset");
+    while (rfnoc_chdr_rst) @(posedge rfnoc_chdr_clk);
+    test.end_timeout(timeout);
+    test.assert_error(!rfnoc_chdr_rst, "Reset did not deassert");
+    test.end_test();
+
+    test.start_test("Flush block then reset it");
+    begin
+      test.start_timeout(timeout, 10us, "Waiting for flush_and_reset");
+      blk_ctrl.flush_and_reset();
+      test.end_timeout(timeout);
+    end
+    test.end_test();
+
+    test.start_test("Read Block Info");
+    begin
+      logic [31:0] noc_id;
+      logic  [5:0] num_data_i;
+      logic  [5:0] num_data_o;
+      logic  [5:0] ctrl_fifosize;
+      logic  [5:0] mtu;
+      test.start_timeout(timeout, 1us, "Waiting for block info response");
+      // Get static block info and validate it
+      blk_ctrl.get_block_info(noc_id, num_data_i, num_data_o, ctrl_fifosize, mtu);
+      test.assert_error(noc_id == 1, "Incorrect noc_id Value");
+      test.assert_error(num_data_i == 2, "Incorrect num_data_i Value");
+      test.assert_error(num_data_o == 2, "Incorrect num_data_o Value");
+      test.assert_error(ctrl_fifosize == 1, "Incorrect ctrl_fifosize Value");
+      test.assert_error(mtu == 10, "Incorrect mtu Value");
+
+      // Read status register and validate it
+      blk_ctrl.reg_read(dut.REG_CTRL_STATUS, rvalue);
+      test.assert_error(rvalue[31:24] == 2, "Incorrect NSPC Value");
+      test.assert_error(rvalue[23:16] == 32, "Incorrect SAMP_W Value");
+      test.end_timeout(timeout);
+    end
+    test.end_test();
+
+    test.start_test("Stream Data Through Loopback Port");
+    begin
+      // Send and receive packets
+      repeat (NUM_PKTS) begin
+        chdr_word_t tx_data[$];
+        chdr_word_t rx_data[$];
+        test.start_timeout(timeout, 5us, "Waiting for pkt to loop back");
+        tx_data.delete();
+        for (int i = 0; i < LPP; i++)
+          tx_data.push_back({$urandom(), $urandom()});
+        blk_ctrl.send(PORT_LOOP, tx_data);
+        blk_ctrl.recv(PORT_LOOP, rx_data);
+        test.assert_error(blk_ctrl.compare_data(tx_data, rx_data), "Data mismatch");
+        test.end_timeout(timeout);
+      end
+
+      // Read sample and packet counts on loopback port
+      blk_ctrl.reg_read(dut.REG_LOOP_LINE_CNT_LO, rvalue);
+      test.assert_error(rvalue == (LPP*NUM_PKTS), "Incorrect REG_LOOP_LINE_CNT_LO value");
+      blk_ctrl.reg_read(dut.REG_LOOP_PKT_CNT_LO, rvalue);
+      test.assert_error(rvalue == NUM_PKTS, "Incorrect REG_LOOP_PKT_CNT_LO value");
+
+      // Read sample and packet counts on source port
+      blk_ctrl.reg_read(dut.REG_SRC_LINE_CNT_LO, rvalue);
+      test.assert_error(rvalue == 0, "Incorrect REG_SRC_LINE_CNT_LO value");
+      blk_ctrl.reg_read(dut.REG_SRC_PKT_CNT_LO, rvalue);
+      test.assert_error(rvalue == 0, "Incorrect REG_SRC_PKT_CNT_LO value");
+
+      // Read sample and packet counts on sink port
+      blk_ctrl.reg_read(dut.REG_SNK_LINE_CNT_LO, rvalue);
+      test.assert_error(rvalue == 0, "Incorrect REG_SNK_LINE_CNT_LO value");
+      blk_ctrl.reg_read(dut.REG_SNK_PKT_CNT_LO, rvalue);
+      test.assert_error(rvalue == 0, "Incorrect REG_SNK_PKT_CNT_LO value");
+    end
+    test.end_test();
+
+    test.start_test("Stream Data To Sink Port");
+    begin
+      // Send packets
+      repeat (NUM_PKTS) begin
+        chdr_word_t tx_data[$];
+        chdr_word_t rx_data[$];
+        test.start_timeout(timeout, 5us, "Waiting for pkt to loop back");
+        tx_data.delete();
+        for (int i = 0; i < LPP; i++)
+          tx_data.push_back({$urandom(), $urandom()});
+        blk_ctrl.send(PORT_SRCSNK, tx_data);
+        test.end_timeout(timeout);
+      end
+      repeat (NUM_PKTS * LPP * 4) @(posedge rfnoc_chdr_clk);
+
+      // Read sample and packet counts on loopback port
+      blk_ctrl.reg_read(dut.REG_LOOP_LINE_CNT_LO, rvalue);
+      test.assert_error(rvalue == (LPP*NUM_PKTS), "Incorrect REG_LOOP_LINE_CNT_LO value");
+      blk_ctrl.reg_read(dut.REG_LOOP_PKT_CNT_LO, rvalue);
+      test.assert_error(rvalue == NUM_PKTS, "Incorrect REG_LOOP_PKT_CNT_LO value");
+
+      // Read sample and packet counts on source port
+      blk_ctrl.reg_read(dut.REG_SRC_LINE_CNT_LO, rvalue);
+      test.assert_error(rvalue == 0, "Incorrect REG_SRC_LINE_CNT_LO value");
+      blk_ctrl.reg_read(dut.REG_SRC_PKT_CNT_LO, rvalue);
+      test.assert_error(rvalue == 0, "Incorrect REG_SRC_PKT_CNT_LO value");
+
+      // Read sample and packet counts on sink port
+      blk_ctrl.reg_read(dut.REG_SNK_LINE_CNT_LO, rvalue);
+      test.assert_error(rvalue == (LPP*NUM_PKTS), "Incorrect REG_SNK_LINE_CNT_LO value");
+      blk_ctrl.reg_read(dut.REG_SNK_PKT_CNT_LO, rvalue);
+      test.assert_error(rvalue == NUM_PKTS, "Incorrect REG_SNK_PKT_CNT_LO value");
+    end
+    test.end_test();
+
+    test.start_test("Stream Data From Source Port");
+    begin
+      // Turn on the source for some time then stop it
+      blk_ctrl.reg_write(dut.REG_SRC_LINES_PER_PKT, LPP-1);
+      blk_ctrl.reg_write(dut.REG_SRC_BYTES_PER_PKT, (LPP+1)*8);
+      blk_ctrl.reg_write(dut.REG_CTRL_STATUS, 2'b10);
+      repeat ((NUM_PKTS / 10) * LPP) @(posedge rfnoc_chdr_clk);
+      blk_ctrl.reg_write(dut.REG_CTRL_STATUS, 2'b00);
+      blk_ctrl.reg_read(dut.REG_SRC_PKT_CNT_LO, rvalue);
+      repeat (rvalue * LPP * 2) @(posedge rfnoc_chdr_clk);
+      blk_ctrl.reg_read(dut.REG_SRC_PKT_CNT_LO, rvalue);
+
+      // Gather the accumulated packets and verify contents
+      for (int p = 0; p < rvalue; p++) begin
+        chdr_word_t exp_data[$];
+        chdr_word_t rx_data[$];
+        test.start_timeout(timeout, 5us, "Waiting for pkt to arrive");
+        exp_data.delete();
+        for (int i = p*LPP; i < (p+1)*LPP; i++)
+          exp_data.push_back({~i[15:0], i[15:0], ~i[15:0], i[15:0]});
+        blk_ctrl.recv(PORT_SRCSNK, rx_data);
+        test.assert_error(blk_ctrl.compare_data(exp_data, rx_data), "Data mismatch");
+        test.end_timeout(timeout);
+      end
+    end
+    test.end_test();
+
+    test.start_test("Clear Counts");
+    begin
+      test.start_timeout(timeout, 1us, "Waiting for clear and readbacks");
+      // Clear
+      blk_ctrl.reg_write(dut.REG_CTRL_STATUS, 2'b01);
+
+      // Read sample and packet counts on loopback port
+      blk_ctrl.reg_read(dut.REG_LOOP_LINE_CNT_LO, rvalue);
+      test.assert_error(rvalue == 0, "Incorrect REG_LOOP_LINE_CNT_LO value");
+      blk_ctrl.reg_read(dut.REG_LOOP_PKT_CNT_LO, rvalue);
+      test.assert_error(rvalue == 0, "Incorrect REG_LOOP_PKT_CNT_LO value");
+
+      // Read sample and packet counts on source port
+      blk_ctrl.reg_read(dut.REG_SRC_LINE_CNT_LO, rvalue);
+      test.assert_error(rvalue == 0, "Incorrect REG_SRC_LINE_CNT_LO value");
+      blk_ctrl.reg_read(dut.REG_SRC_PKT_CNT_LO, rvalue);
+      test.assert_error(rvalue == 0, "Incorrect REG_SRC_PKT_CNT_LO value");
+
+      // Read sample and packet counts on sink port
+      blk_ctrl.reg_read(dut.REG_SNK_LINE_CNT_LO, rvalue);
+      test.assert_error(rvalue == 0, "Incorrect REG_SNK_LINE_CNT_LO value");
+      blk_ctrl.reg_read(dut.REG_SNK_PKT_CNT_LO, rvalue);
+      test.assert_error(rvalue == 0, "Incorrect REG_SNK_PKT_CNT_LO value");
+      test.end_timeout(timeout);
+    end
+    test.end_test();
+
+    // Finish Up
+    // ----------------------------------------
+    // Display final statistics and results
+    test.end_tb();
+  end
+
+endmodule
---
+++ usrp3/lib/erfnoc/utils/noc_shell_generic_ctrlport_raw.v
@@ -0,0 +1,282 @@
+//
+// Copyright 2019 Ettus Research, A National Instruments Company
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+// Module: noc_shell_generic_ctrlport_raw
+// Description:
+//
+// Parameters:
+//
+// Signals:
+
+module noc_shell_generic_ctrlport_raw #(
+  parameter [31:0] NOC_ID          = 32'h0,
+  parameter  [9:0] THIS_PORTID     = 10'd0,
+  parameter        CHDR_W          = 64,
+  parameter  [5:0] CTRL_FIFOSIZE   = 0,
+  parameter  [0:0] CTRLPORT_SLV_EN = 1,
+  parameter  [5:0] NUM_DATA_I      = 0,
+  parameter  [5:0] NUM_DATA_O      = 0,
+  parameter        SAMP_W          = 32,
+  parameter        NSPC            = 2,
+  parameter  [5:0] MTU             = 0,
+  parameter        CTXT_FIFOSIZE   = 1,
+  parameter        PYLD_FIFOSIZE   = 1
+)(
+  // Framework Interface
+  //------------------------------------------------------------
+  // RFNoC Framework Clocks and Resets
+  input  wire                                 rfnoc_chdr_clk,
+  input  wire                                 rfnoc_chdr_rst,
+  input  wire                                 rfnoc_ctrl_clk,
+  input  wire                                 rfnoc_ctrl_rst,
+  // RFNoC Backend Interface                  
+  input  wire [511:0]                         rfnoc_core_config,
+  output wire [511:0]                         rfnoc_core_status,
+  // CHDR Input Ports (from framework)        
+  input  wire [(CHDR_W*NUM_DATA_I)-1:0]       s_rfnoc_chdr_tdata,
+  input  wire [NUM_DATA_I-1:0]                s_rfnoc_chdr_tlast,
+  input  wire [NUM_DATA_I-1:0]                s_rfnoc_chdr_tvalid,
+  output wire [NUM_DATA_I-1:0]                s_rfnoc_chdr_tready,
+  // CHDR Output Ports (to framework)         
+  output wire [(CHDR_W*NUM_DATA_O)-1:0]       m_rfnoc_chdr_tdata,
+  output wire [NUM_DATA_O-1:0]                m_rfnoc_chdr_tlast,
+  output wire [NUM_DATA_O-1:0]                m_rfnoc_chdr_tvalid,
+  input  wire [NUM_DATA_O-1:0]                m_rfnoc_chdr_tready,
+  // AXIS-Ctrl Input Port (from framework)
+  input  wire [31:0]                          s_rfnoc_ctrl_tdata,
+  input  wire                                 s_rfnoc_ctrl_tlast,
+  input  wire                                 s_rfnoc_ctrl_tvalid,
+  output wire                                 s_rfnoc_ctrl_tready,
+  // AXIS-Ctrl Output Port (to framework)
+  output wire [31:0]                          m_rfnoc_ctrl_tdata,
+  output wire                                 m_rfnoc_ctrl_tlast,
+  output wire                                 m_rfnoc_ctrl_tvalid,
+  input  wire                                 m_rfnoc_ctrl_tready,
+
+  // Client Interface
+  //------------------------------------------------------------
+  // Client Clocks and Resets
+  output wire                                 chdr_clk,
+  output wire                                 chdr_rst,
+  output wire                                 ctrl_clk,
+  output wire                                 ctrl_rst,
+  // Control Port Master (Request)            
+  output wire                                 m_ctrlport_req_wr,
+  output wire                                 m_ctrlport_req_rd,
+  output wire [19:0]                          m_ctrlport_req_addr,
+  output wire [31:0]                          m_ctrlport_req_data,
+  output wire [3:0]                           m_ctrlport_req_byte_en,
+  output wire                                 m_ctrlport_req_has_time,
+  output wire [63:0]                          m_ctrlport_req_time,
+  input  wire                                 m_ctrlport_resp_ack,
+  input  wire [1:0]                           m_ctrlport_resp_status,
+  input  wire [31:0]                          m_ctrlport_resp_data,
+  // Control Port Slave (Request)             
+  input  wire                                 s_ctrlport_req_wr,
+  input  wire                                 s_ctrlport_req_rd,
+  input  wire [19:0]                          s_ctrlport_req_addr,
+  input  wire [9:0]                           s_ctrlport_req_portid,
+  input  wire [15:0]                          s_ctrlport_req_rem_epid,
+  input  wire [9:0]                           s_ctrlport_req_rem_portid,
+  input  wire [31:0]                          s_ctrlport_req_data,
+  input  wire [3:0]                           s_ctrlport_req_byte_en,
+  input  wire                                 s_ctrlport_req_has_time,
+  input  wire [63:0]                          s_ctrlport_req_time,
+  output wire                                 s_ctrlport_resp_ack,
+  output wire [1:0]                           s_ctrlport_resp_status,
+  output wire [31:0]                          s_ctrlport_resp_data,
+  // Payload stream out (to user logic)
+  output wire [(SAMP_W*NSPC*NUM_DATA_I)-1:0]  m_axis_payload_tdata,
+  output wire [(NSPC*NUM_DATA_I)-1:0]         m_axis_payload_tkeep,
+  output wire [NUM_DATA_I-1:0]                m_axis_payload_tlast,
+  output wire [NUM_DATA_I-1:0]                m_axis_payload_tvalid,
+  input  wire [NUM_DATA_I-1:0]                m_axis_payload_tready,
+  // Context stream out (to user logic)
+  output wire [(CHDR_W*NUM_DATA_I)-1:0]       m_axis_context_tdata,
+  output wire [(4*NUM_DATA_I)-1:0]            m_axis_context_tuser,
+  output wire [NUM_DATA_I-1:0]                m_axis_context_tlast,
+  output wire [NUM_DATA_I-1:0]                m_axis_context_tvalid,
+  input  wire [NUM_DATA_I-1:0]                m_axis_context_tready,
+  // Payload stream in (from user logic)
+  input  wire [(SAMP_W*NSPC*NUM_DATA_O)-1:0]  s_axis_payload_tdata,
+  input  wire [(NSPC*NUM_DATA_O)-1:0]         s_axis_payload_tkeep,
+  input  wire [NUM_DATA_O-1:0]                s_axis_payload_tlast,
+  input  wire [NUM_DATA_O-1:0]                s_axis_payload_tvalid,
+  output wire [NUM_DATA_O-1:0]                s_axis_payload_tready,
+  // Context stream in (from user logic)
+  input  wire [(CHDR_W*NUM_DATA_O)-1:0]       s_axis_context_tdata,
+  input  wire [(4*NUM_DATA_O)-1:0]            s_axis_context_tuser,
+  input  wire [NUM_DATA_O-1:0]                s_axis_context_tlast,
+  input  wire [NUM_DATA_O-1:0]                s_axis_context_tvalid,
+  output wire [NUM_DATA_O-1:0]                s_axis_context_tready
+);
+
+  // ---------------------------------------------------
+  //  Backend Interface
+  // ---------------------------------------------------
+  wire         data_i_flush_en;
+  wire [31:0]  data_i_flush_timeout;
+  wire [63:0]  data_i_flush_active;
+  wire [63:0]  data_i_flush_done;
+  wire         data_o_flush_en;
+  wire [31:0]  data_o_flush_timeout;
+  wire [63:0]  data_o_flush_active;
+  wire [63:0]  data_o_flush_done;
+
+  backend_iface #(
+    .NOC_ID               (NOC_ID       ),
+    .NUM_DATA_I           (NUM_DATA_I   ),
+    .NUM_DATA_O           (NUM_DATA_O   ),
+    .CTRL_FIFOSIZE        (CTRL_FIFOSIZE),
+    .MTU                  (MTU          )
+  ) backend_iface_i (
+    .rfnoc_chdr_clk_i     (rfnoc_chdr_clk      ),
+    .rfnoc_chdr_rst_i     (rfnoc_chdr_rst      ),
+    .rfnoc_ctrl_clk_i     (rfnoc_ctrl_clk      ),
+    .rfnoc_ctrl_rst_i     (rfnoc_ctrl_rst      ),
+    .rfnoc_core_config    (rfnoc_core_config   ),
+    .rfnoc_core_status    (rfnoc_core_status   ),
+    .rfnoc_chdr_clk_o     (chdr_clk            ),
+    .rfnoc_chdr_rst_o     (chdr_rst            ),
+    .rfnoc_ctrl_clk_o     (ctrl_clk            ),
+    .rfnoc_ctrl_rst_o     (ctrl_rst            ),
+    .data_i_flush_en      (data_i_flush_en     ),
+    .data_i_flush_timeout (data_i_flush_timeout),
+    .data_i_flush_active  (data_i_flush_active ),
+    .data_i_flush_done    (data_i_flush_done   ),
+    .data_o_flush_en      (data_o_flush_en     ),
+    .data_o_flush_timeout (data_o_flush_timeout),
+    .data_o_flush_active  (data_o_flush_active ),
+    .data_o_flush_done    (data_o_flush_done   )
+  );
+
+  // ---------------------------------------------------
+  //  Control Path
+  // ---------------------------------------------------
+
+  ctrlport_endpoint #(
+    .THIS_PORTID              (THIS_PORTID    ),
+    .SYNC_CLKS                (0              ),
+    .AXIS_CTRL_MST_EN         (CTRLPORT_SLV_EN),
+    .AXIS_CTRL_SLV_EN         (1              ),
+    .SLAVE_FIFO_SIZE          (CTRL_FIFOSIZE  )
+  ) ctrlport_ep_i (
+    .rfnoc_ctrl_clk           (ctrl_clk                 ),
+    .rfnoc_ctrl_rst           (ctrl_rst                 ),
+    .ctrlport_clk             (chdr_clk                 ),
+    .ctrlport_rst             (chdr_rst                 ),
+    .s_rfnoc_ctrl_tdata       (s_rfnoc_ctrl_tdata       ),
+    .s_rfnoc_ctrl_tlast       (s_rfnoc_ctrl_tlast       ),
+    .s_rfnoc_ctrl_tvalid      (s_rfnoc_ctrl_tvalid      ),
+    .s_rfnoc_ctrl_tready      (s_rfnoc_ctrl_tready      ),
+    .m_rfnoc_ctrl_tdata       (m_rfnoc_ctrl_tdata       ),
+    .m_rfnoc_ctrl_tlast       (m_rfnoc_ctrl_tlast       ),
+    .m_rfnoc_ctrl_tvalid      (m_rfnoc_ctrl_tvalid      ),
+    .m_rfnoc_ctrl_tready      (m_rfnoc_ctrl_tready      ),
+    .m_ctrlport_req_wr        (m_ctrlport_req_wr        ),
+    .m_ctrlport_req_rd        (m_ctrlport_req_rd        ),
+    .m_ctrlport_req_addr      (m_ctrlport_req_addr      ),
+    .m_ctrlport_req_data      (m_ctrlport_req_data      ),
+    .m_ctrlport_req_byte_en   (m_ctrlport_req_byte_en   ),
+    .m_ctrlport_req_has_time  (m_ctrlport_req_has_time  ),
+    .m_ctrlport_req_time      (m_ctrlport_req_time      ),
+    .m_ctrlport_resp_ack      (m_ctrlport_resp_ack      ),
+    .m_ctrlport_resp_status   (m_ctrlport_resp_status   ),
+    .m_ctrlport_resp_data     (m_ctrlport_resp_data     ),
+    .s_ctrlport_req_wr        (s_ctrlport_req_wr        ),
+    .s_ctrlport_req_rd        (s_ctrlport_req_rd        ),
+    .s_ctrlport_req_addr      (s_ctrlport_req_addr      ),
+    .s_ctrlport_req_portid    (s_ctrlport_req_portid    ),
+    .s_ctrlport_req_rem_epid  (s_ctrlport_req_rem_epid  ),
+    .s_ctrlport_req_rem_portid(s_ctrlport_req_rem_portid),
+    .s_ctrlport_req_data      (s_ctrlport_req_data      ),
+    .s_ctrlport_req_byte_en   (s_ctrlport_req_byte_en   ),
+    .s_ctrlport_req_has_time  (s_ctrlport_req_has_time  ),
+    .s_ctrlport_req_time      (s_ctrlport_req_time      ),
+    .s_ctrlport_resp_ack      (s_ctrlport_resp_ack      ),
+    .s_ctrlport_resp_status   (s_ctrlport_resp_status   ),
+    .s_ctrlport_resp_data     (s_ctrlport_resp_data     )
+  );
+
+  // ---------------------------------------------------
+  //  Data Path
+  // ---------------------------------------------------
+
+  genvar i;
+  generate 
+    for (i = 0; i < NUM_DATA_I; i = i + 1) begin: in
+      chdr_to_axis_raw_data #(
+        .CHDR_W               (CHDR_W        ),
+        .SAMP_W               (SAMP_W        ),
+        .NSPC                 (NSPC          ),
+        .SYNC_CLKS            (0             ),
+        .CONTEXT_FIFO_SIZE    (CTXT_FIFOSIZE),
+        .PAYLOAD_FIFO_SIZE    (PYLD_FIFOSIZE),
+        .CONTEXT_PREFETCH_EN  (1             )
+      ) chdr2raw_i (
+        .axis_chdr_clk        (chdr_clk                                             ),
+        .axis_chdr_rst        (chdr_rst                                             ),
+        .axis_data_clk        (chdr_clk                                             ),
+        .axis_data_rst        (chdr_rst                                             ),
+        .s_axis_chdr_tdata    (s_rfnoc_chdr_tdata   [(i*CHDR_W)+:CHDR_W]            ),
+        .s_axis_chdr_tlast    (s_rfnoc_chdr_tlast   [i]                             ),
+        .s_axis_chdr_tvalid   (s_rfnoc_chdr_tvalid  [i]                             ),
+        .s_axis_chdr_tready   (s_rfnoc_chdr_tready  [i]                             ),
+        .m_axis_payload_tdata (m_axis_payload_tdata [(i*SAMP_W*NSPC)+:(SAMP_W*NSPC)]),
+        .m_axis_payload_tkeep (m_axis_payload_tkeep [(i*NSPC)+:NSPC]                ),
+        .m_axis_payload_tlast (m_axis_payload_tlast [i]                             ),
+        .m_axis_payload_tvalid(m_axis_payload_tvalid[i]                             ),
+        .m_axis_payload_tready(m_axis_payload_tready[i]                             ),
+        .m_axis_context_tdata (m_axis_context_tdata [(i*CHDR_W)+:(CHDR_W)]          ),
+        .m_axis_context_tuser (m_axis_context_tuser [(i*4)+:4]                      ),
+        .m_axis_context_tlast (m_axis_context_tlast [i]                             ),
+        .m_axis_context_tvalid(m_axis_context_tvalid[i]                             ),
+        .m_axis_context_tready(m_axis_context_tready[i]                             ),
+        .flush_en             (data_i_flush_en                                      ),
+        .flush_timeout        (data_i_flush_timeout                                 ),
+        .flush_active         (data_i_flush_active  [i]                             ),
+        .flush_done           (data_i_flush_done    [i]                             )
+      );
+    end
+
+    for (i = 0; i < NUM_DATA_O; i = i + 1) begin: out
+      axis_raw_data_to_chdr #(
+        .CHDR_W               (CHDR_W        ),
+        .SAMP_W               (SAMP_W        ),
+        .NSPC                 (NSPC          ),
+        .SYNC_CLKS            (0             ),
+        .CONTEXT_FIFO_SIZE    (CTXT_FIFOSIZE ),
+        .PAYLOAD_FIFO_SIZE    (PYLD_FIFOSIZE ),
+        .CONTEXT_PREFETCH_EN  (1             ),
+        .MTU                  (MTU           )
+      ) raw2chdr_i (
+        .axis_chdr_clk        (chdr_clk                                             ),
+        .axis_chdr_rst        (chdr_rst                                             ),
+        .axis_data_clk        (chdr_clk                                             ),
+        .axis_data_rst        (chdr_rst                                             ),
+        .m_axis_chdr_tdata    (m_rfnoc_chdr_tdata   [(i*CHDR_W)+:CHDR_W]            ),
+        .m_axis_chdr_tlast    (m_rfnoc_chdr_tlast   [i]                             ),
+        .m_axis_chdr_tvalid   (m_rfnoc_chdr_tvalid  [i]                             ),
+        .m_axis_chdr_tready   (m_rfnoc_chdr_tready  [i]                             ),
+        .s_axis_payload_tdata (s_axis_payload_tdata [(i*SAMP_W*NSPC)+:(SAMP_W*NSPC)]),
+        .s_axis_payload_tkeep (s_axis_payload_tkeep [(i*NSPC)+:NSPC]                ),
+        .s_axis_payload_tlast (s_axis_payload_tlast [i]                             ),
+        .s_axis_payload_tvalid(s_axis_payload_tvalid[i]                             ),
+        .s_axis_payload_tready(s_axis_payload_tready[i]                             ),
+        .s_axis_context_tdata (s_axis_context_tdata [(i*CHDR_W)+:(CHDR_W)]          ),
+        .s_axis_context_tuser (s_axis_context_tuser [(i*4)+:4]                      ),
+        .s_axis_context_tlast (s_axis_context_tlast [i]                             ),
+        .s_axis_context_tvalid(s_axis_context_tvalid[i]                             ),
+        .s_axis_context_tready(s_axis_context_tready[i]                             ),
+        .framer_errors        (                                                     ),
+        .flush_en             (data_o_flush_en                                      ),
+        .flush_timeout        (data_o_flush_timeout                                 ),
+        .flush_active         (data_o_flush_active  [i]                             ),
+        .flush_done           (data_o_flush_done    [i]                             )
+      );
+    end
+  endgenerate
+
+endmodule // noc_shell_generic_ctrlport_raw
