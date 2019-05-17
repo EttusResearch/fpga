0764756fe9c7caa5ad428683721556c37d112bf2
Wade Fife <wade.fife@ettus.com>
2019-05-17 16:35:05 -0500

    rfnoc: Fix pulse_stretch when SCALE is power-of-2

--- usrp3/lib/control/pulse_stretch.v
+++ usrp3/lib/control/pulse_stretch.v
@@ -3,6 +3,40 @@
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 //
+// Module:  pulse_stretch
+//
+// Description:
+//
+//   Pulse stretcher. Takes any input pulse that is SCALE+2 clock cycles or 
+//   less and outputs a pulse that is SCALE+1 clock cycles. However, if an 
+//   input pulse is longer than SCALE+2 clock cycles then the output pulse 
+//   repeats. If more than one pulse is input within SCALE+2 clock cycles then 
+//   additional pulses will be ignored.
+//
+// Examples (SCALE = 2):
+//
+//   Clock            _/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_
+//
+//
+//   pulse            _/‾‾‾\_______________/‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\___________________
+//
+//   pulse_stretched  _____/‾‾‾‾‾‾‾‾‾‾‾\_______/‾‾‾‾‾‾‾‾‾‾‾\___________________
+//
+//
+//   pulse            _/‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\___________________________________
+//
+//   pulse_stretched  _____/‾‾‾‾‾‾‾‾‾‾‾\___/‾‾‾‾‾‾‾‾‾‾‾\_______________________
+//
+//
+//   pulse            _/‾‾‾\_______/‾‾‾\___/‾‾‾\_______/‾‾‾‾‾‾‾\_______________
+//
+//   pulse_stretched  _____/‾‾‾‾‾‾‾‾‾‾‾\_______/‾‾‾‾‾‾‾‾‾‾‾\___/‾‾‾‾‾‾‾‾‾‾‾\___
+
+// Parameters:
+//
+//   SCALE : The number of clock cycles to add to a single cycle pulse. Or, the
+//           number of clock cycles, minus 1, for the output pulse.
+//
 
 module pulse_stretch #(
   parameter SCALE = 64'd12_500_000
@@ -13,7 +47,7 @@ module pulse_stretch #(
   output pulse_stretched
 );
 
-  reg [$clog2(SCALE)-1:0] count = 'd0;
+  reg [$clog2(SCALE+1)-1:0] count = 'd0;
   reg             state = 1'b0;
 
   always @ (posedge clk)
